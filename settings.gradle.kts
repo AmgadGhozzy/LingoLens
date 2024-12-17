3d9c51b904cbc59b742f4dd95f80e23ca4004ac3
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2024-12-17 23:31:55 +0200

    feat: Initial commit of LingoLens project

---
+++ settings.gradle.kts
@@ -0,0 +1,33 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "LingoLens"
+
+include(":app")
+
+
+include(":features:translation")
+include(":features:phrase")
+include(":features:ocr")
+
+include(":core:ui")
+include(":core:data")
+
