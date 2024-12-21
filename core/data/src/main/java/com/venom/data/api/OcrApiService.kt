ab335c76782ff1e19119382247c259bc878fa268
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2024-12-21 22:31:18 +0200

    feat: Add OCR API service interface

---
+++ core/data/src/main/java/com/venom/data/api/OcrApiService.kt
@@ -0,0 +1,35 @@
+package com.venom.data.api
+
+import com.venom.data.model.OcrResponse
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.Header
+import retrofit2.http.Multipart
+import retrofit2.http.POST
+import retrofit2.http.Part
+
+interface OcrApiService {
+    @Multipart
+    @POST("v2/ocr/ocr")
+    suspend fun performOcr(
+        @Header("authorization") authToken: String,
+        @Part("show_original_response") showOriginalResponse: RequestBody,
+        @Part("fallback_providers") fallbackProviders: RequestBody,
+        @Part("providers") providers: RequestBody,
+        @Part file: MultipartBody.Part,
+        @Part("language") language: RequestBody
+    ): Response<OcrResponse>
+
+    companion object {
+        private const val BASE_URL = "https://api.edenai.run/"
+
+        fun create(): OcrApiService {
+            return Retrofit.Builder().baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create()).build()
+                .create(OcrApiService::class.java)
+        }
+    }
+}
