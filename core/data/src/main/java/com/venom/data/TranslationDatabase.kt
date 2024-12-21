8c7db23573b936bf72f8f3c4e31c099aa0a0176b
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2024-12-21 22:17:54 +0200

    feat: Add Room database for translations

---
+++ core/data/src/main/java/com/venom/data/TranslationDatabase.kt
@@ -0,0 +1,13 @@
+package com.venom.data
+
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.venom.data.local.dao.TranslationDao
+import com.venom.data.local.typeconverter.ListStringConverter
+import com.venom.data.model.TranslationEntry
+
+@androidx.room.Database(entities = [TranslationEntry::class], version = 1, exportSchema = false)
+@TypeConverters(ListStringConverter::class)
+abstract class TranslationDatabase : RoomDatabase() {
+    abstract fun translationDao(): TranslationDao
+}
