f67ec02843c1aac69faf1bdd427c11464b0d7fd8
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2024-12-25 23:54:48 +0200

    Fix: Use correct annotation for TypeConverter
    Changed @androidx.room.TypeConverter to @TypeConverter in ListStringConverter.

--- core/data/src/main/java/com/venom/data/local/typeconverter/ListStringConverter.kt
+++ core/data/src/main/java/com/venom/data/local/typeconverter/ListStringConverter.kt
@@ -1,16 +1,17 @@
 package com.venom.data.local.typeconverter
 
+import androidx.room.TypeConverter
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 
 object ListStringConverter {
 
-    @androidx.room.TypeConverter
+    @TypeConverter
     fun fromList(list: List<String>?): String? {
         return Gson().toJson(list)
     }
 
-    @androidx.room.TypeConverter
+    @TypeConverter
     fun toList(value: String?): List<String>? {
         val listType = object : TypeToken<List<String>?>() {}.type
         return Gson().fromJson(value, listType)
