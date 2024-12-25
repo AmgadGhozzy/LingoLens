89259397749387930ef87a70775b4740a6f08abd
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2024-12-26 00:00:50 +0200

    feat: Implement History and Bookmark dialogs in LingoLensApp
    Updated LingoLensApp to show BookmarkHistoryScreen in dialogs when history or bookmark icons in top bar is clicked, and also added settings icon, also wrapped bottom navigation bar in a custom card for better UI

--- app/src/main/java/com/venom/lingolens/ui/LingoLensApp.kt
+++ app/src/main/java/com/venom/lingolens/ui/LingoLensApp.kt
@@ -1,5 +1,6 @@
 package com.venom.lingolens.ui
 
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Bookmark
@@ -9,50 +10,71 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.rememberNavController
 import com.venom.lingolens.navigation.NavigationGraph
 import com.venom.lingolens.navigation.Screen
+import com.venom.lingopro.ui.screens.BookmarkHistoryScreen
+import com.venom.lingopro.ui.viewmodel.ViewType
 import com.venom.resources.R
-import com.venom.textsnap.ui.screens.OcrViewModel
+import com.venom.textsnap.ui.viewmodel.OcrViewModel
 import com.venom.ui.components.bars.TopBar
 import com.venom.ui.components.buttons.CustomButton
+import com.venom.ui.components.dialogs.CustomCard
 
 @Composable
 fun LingoLensApp(
-    ocrViewModel: OcrViewModel,
-    startCamera: () -> Unit, imageSelector: () -> Unit) {
+    ocrViewModel: OcrViewModel, startCamera: () -> Unit, imageSelector: () -> Unit
+) {
     val navController = rememberNavController()
     var selectedScreen: Screen by remember { mutableStateOf(Screen.Translation) }
+    var showHistoryDialog by remember { mutableStateOf(false) }
+    var showBookmarkDialog by remember { mutableStateOf(false) }
 
-    Scaffold(
-        topBar = {
-            TopBar(
-                title = stringResource(id = R.string.app_name),
-                onNavigationClick = {
-                    navController.navigate(Screen.Translation.route) {
-                        popUpTo(navController.graph.startDestinationId)
-                        launchSingleTop = true
-                    }
+
+    if (showHistoryDialog) BookmarkHistoryScreen(
+        onBackClick = { showHistoryDialog = false }, viewType = ViewType.HISTORY
+    )
+    else if (showBookmarkDialog) BookmarkHistoryScreen(
+        onBackClick = { showBookmarkDialog = false }, viewType = ViewType.BOOKMARKS
+    )
+    else Scaffold(topBar = {
+        TopBar(title = stringResource(id = R.string.app_name), onNavigationClick = {
+            navController.navigate(Screen.Translation.route) {
+                popUpTo(navController.graph.startDestinationId)
+                launchSingleTop = true
+            }
+        }, actions = {
+            CustomButton(icon = R.drawable.icon_history,
+                contentDescription = stringResource(R.string.history_title),
+                onClick = {
+                    showHistoryDialog = true
+                })
+            CustomButton(icon = R.drawable.icon_bookmark_filled,
+                contentDescription = stringResource(R.string.bookmarks_title),
+                onClick = {
+                    showBookmarkDialog = true
+
+                })
+            CustomButton(icon = R.drawable.icon_settings,
+                contentDescription = stringResource(R.string.bookmarks_title),
+                onClick = {
+
+                })
+        })
+    }, bottomBar = {
+        CustomCard {
+            NavigationBar(
+                modifier = Modifier.height(64.dp),
+            ) {
+                NavigationBarItem(icon = {
+                    Icon(
+                        Icons.Default.Bookmark,
+                        contentDescription = stringResource(R.string.bookmarks_title)
+                    )
                 },
-                actions = {
-                    CustomButton(
-                        icon = R.drawable.icon_history,
-                        contentDescription = stringResource(R.string.history_title),
-                        onClick = { }
-                    )
-                    CustomButton(
-                        icon = R.drawable.icon_bookmark_filled,
-                        contentDescription = stringResource(R.string.bookmarks_title),
-                        onClick = { }
-                    )
-                }
-            )
-        },
-        bottomBar = {
-            NavigationBar {
-                NavigationBarItem(
-                    icon = { Icon(Icons.Default.Bookmark, contentDescription = stringResource(R.string.bookmarks_title)) },
                     label = { Text(stringResource(R.string.bookmarks_title)) },
+                    alwaysShowLabel = false,
                     selected = selectedScreen == Screen.Bookmarks,
                     onClick = {
                         selectedScreen = Screen.Bookmarks
@@ -60,12 +82,16 @@ fun LingoLensApp(
                             popUpTo(navController.graph.startDestinationId)
                             launchSingleTop = true
                         }
-                    }
-                )
+                    })
 
-                NavigationBarItem(
-                    icon = { Icon(Icons.Default.Camera, contentDescription = stringResource(R.string.ocr_title)) },
+                NavigationBarItem(icon = {
+                    Icon(
+                        Icons.Default.Camera,
+                        contentDescription = stringResource(R.string.ocr_title)
+                    )
+                },
                     label = { Text(stringResource(R.string.ocr_title)) },
+                    alwaysShowLabel = false,
                     selected = selectedScreen == Screen.Ocr,
                     onClick = {
                         selectedScreen = Screen.Ocr
@@ -73,12 +99,16 @@ fun LingoLensApp(
                             popUpTo(navController.graph.startDestinationId)
                             launchSingleTop = true
                         }
-                    }
-                )
+                    })
 
-                NavigationBarItem(
-                    icon = { Icon(Icons.Default.Translate, contentDescription = stringResource(R.string.translate_title)) },
+                NavigationBarItem(icon = {
+                    Icon(
+                        Icons.Default.Translate,
+                        contentDescription = stringResource(R.string.translate_title)
+                    )
+                },
                     label = { Text(stringResource(R.string.translate_title)) },
+                    alwaysShowLabel = false,
                     selected = selectedScreen == Screen.Translation,
                     onClick = {
                         selectedScreen = Screen.Translation
@@ -86,11 +116,10 @@ fun LingoLensApp(
                             popUpTo(navController.graph.startDestinationId)
                             launchSingleTop = true
                         }
-                    }
-                )
+                    })
             }
         }
-    ) { innerPadding ->
+    }) { innerPadding ->
         NavigationGraph(
             ocrViewModel = ocrViewModel,
             navController = navController,
@@ -99,4 +128,5 @@ fun LingoLensApp(
             imageSelector = imageSelector
         )
     }
+
 }
