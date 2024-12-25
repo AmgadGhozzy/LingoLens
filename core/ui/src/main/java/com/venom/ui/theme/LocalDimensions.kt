07840e6cfc2c94519c9c00b44df5b1d8b348766c
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2024-12-26 00:03:54 +0200

    feat: Add LocalPeekHeight to theme
    Added LocalPeekHeight to theme to provide peek height for components

---
+++ core/ui/src/main/java/com/venom/ui/theme/LocalDimensions.kt
@@ -0,0 +1,7 @@
+package com.venom.ui.theme
+
+import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+val LocalPeekHeight = staticCompositionLocalOf<Dp> { 0.dp }
