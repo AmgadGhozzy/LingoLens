54d844b04f2e575b60461a5ea2709e3d7f54a8ae
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2024-12-25 23:51:11 +0200

    feat: Update Network Module to support multiple Retrofit clients
    The network module was updated to provide two named Retrofit instances, one for OCR and one for translation, using OkHttp client, so it can handle multiple API services

--- core/di/src/main/java/com/venom/di/NetworkModule.kt
+++ core/di/src/main/java/com/venom/di/NetworkModule.kt
@@ -1,12 +1,15 @@
 package com.venom.di
 
+import com.venom.data.api.OcrService
 import com.venom.data.api.TranslationService
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Named
 import javax.inject.Singleton
 
 @Module
@@ -15,9 +18,29 @@ object NetworkModule {
 
     @Provides
     @Singleton
-    fun provideRetrofit(): Retrofit {
-        return Retrofit.Builder().baseUrl(TranslationService.BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create()).build()
+    fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder().build()
     }
 
+    @Provides
+    @Singleton
+    @Named("OcrRetrofit")
+    fun provideOcrRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(OcrService.BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    @Named("TranslationRetrofit")
+    fun provideTranslationRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(TranslationService.BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
 }
