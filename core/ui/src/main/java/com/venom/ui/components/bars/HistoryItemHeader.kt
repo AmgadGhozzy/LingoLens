179d9c30c102d0a8192ffa0b34cea5678d682f45
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2024-12-26 23:45:59 +0200

    feat: Create HistoryItemHeader composable

---
+++ core/ui/src/main/java/com/venom/ui/components/bars/HistoryItemHeader.kt
@@ -0,0 +1,36 @@
+package com.venom.ui.components.bars
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.venom.ui.components.buttons.BookmarkButton
+import com.venom.utils.Extensions.formatTimestamp
+
+
+@Composable
+fun HistoryItemHeader(
+    timestamp: Long,
+    isBookmarked: Boolean,
+    onToggleBookmark: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = timestamp.formatTimestamp(),
+            style = MaterialTheme.typography.labelMedium,
+            color = MaterialTheme.colorScheme.onSurfaceVariant.copy(alpha = 0.5f)
+        )
+        BookmarkButton(
+            isBookmarked = isBookmarked, onToggleBookmark = onToggleBookmark
+        )
+    }
+}
