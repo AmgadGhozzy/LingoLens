b6ea4b9e932af8ebb7cebeb38ad4572135c08f14
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2024-12-27 23:44:53 +0200

    feat: Define ISpeechToTextRepository interface

---
+++ core/domain/src/main/java/com/venom/domain/repo/stt/ISpeechToTextRepository.kt
@@ -0,0 +1,14 @@
+package com.venom.domain.repo.stt
+
+import com.venom.domain.model.SpeechState
+import kotlinx.coroutines.flow.StateFlow
+
+interface ISpeechToTextRepository {
+    val state: StateFlow<SpeechState>
+
+    suspend fun startListening(config: SpeechConfig = SpeechConfig())
+    suspend fun pauseListening()
+    suspend fun stopListening()
+    suspend fun cancelListening()
+    suspend fun destroy()
+}
