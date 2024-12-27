f42ff8b0d3750ee76502573f6fc9dfaa1937d6c2
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2024-12-27 23:12:58 +0200

    Refactor: Improve Navigation and UI Structure
    The code was refactored to:
    
    Separate top and bottom app bars into reusable Composables: LingoLensTopBar and LingoLensBottomBar.
    
    Simplify the main app Composable by removing redundant state management.
    
    Encapsulate navigation logic and screen selection within the app bars.
    
    Convert Screen to ContentType using an extension function for improved readability.

--- app/src/main/java/com/venom/lingolens/ui/LingoLensApp.kt
+++ app/src/main/java/com/venom/lingolens/ui/LingoLensApp.kt
@@ -1,25 +1,22 @@
 package com.venom.lingolens.ui
 
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Bookmark
-import androidx.compose.material.icons.filled.Camera
-import androidx.compose.material.icons.filled.Translate
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import com.venom.lingolens.navigation.LingoLensBottomBar
+import com.venom.lingolens.navigation.LingoLensTopBar
 import com.venom.lingolens.navigation.NavigationGraph
 import com.venom.lingolens.navigation.Screen
-import com.venom.ui.screen.BookmarkHistoryScreen
+import com.venom.lingolens.navigation.navigateToStart
 import com.venom.resources.R
 import com.venom.textsnap.ui.viewmodel.OcrViewModel
 import com.venom.ui.components.bars.TopBar
 import com.venom.ui.components.buttons.CustomButton
-import com.venom.ui.components.dialogs.CustomCard
+import com.venom.ui.screen.BookmarkHistoryScreen
 import com.venom.ui.screen.ContentType
 
 @Composable
@@ -27,106 +24,36 @@ fun LingoLensApp(
     ocrViewModel: OcrViewModel, startCamera: () -> Unit, imageSelector: () -> Unit
 ) {
     val navController = rememberNavController()
-    var selectedScreen: Screen by remember { mutableStateOf(Screen.Translation) }
-    var showHistoryDialog by remember { mutableStateOf(false) }
-    var showBookmarkDialog by remember { mutableStateOf(false) }
+    var selectedScreen by remember { mutableStateOf<Screen>(Screen.Translation) }
+    var showBookmarkHistory by remember { mutableStateOf(false) }
 
+    if (showBookmarkHistory) {
+        BookmarkHistoryScreen(
+            onBackClick = { showBookmarkHistory = false },
+            contentType = selectedScreen.toContentType()
+        )
+        return
+    }
 
-    if (showHistoryDialog) BookmarkHistoryScreen(
-        onBackClick = { showHistoryDialog = false }, contentType = ContentType.TRANSLATION
-    )
-    else if (showBookmarkDialog) BookmarkHistoryScreen(
-        onBackClick = { showBookmarkDialog = false }, contentType = ContentType.OCR
-    )
-    else Scaffold(topBar = {
-        TopBar(title = stringResource(id = R.string.app_name), onNavigationClick = {
-            navController.navigate(Screen.Translation.route) {
-                popUpTo(navController.graph.startDestinationId)
-                launchSingleTop = true
-            }
-        }, actions = {
-            CustomButton(icon = R.drawable.icon_history,
-                contentDescription = stringResource(R.string.history_title),
-                onClick = {
-                    showHistoryDialog = true
-                })
-            CustomButton(icon = R.drawable.icon_bookmark_filled,
-                contentDescription = stringResource(R.string.bookmarks_title),
-                onClick = {
-                    showBookmarkDialog = true
-
-                })
-            CustomButton(icon = R.drawable.icon_settings,
-                contentDescription = stringResource(R.string.bookmarks_title),
-                onClick = {
-
-                })
-        })
+    Scaffold(topBar = {
+        LingoLensTopBar(navController, onBookmarkClick = { showBookmarkHistory = true })
     }, bottomBar = {
-        CustomCard {
-            NavigationBar(
-                modifier = Modifier.height(64.dp),
-            ) {
-                NavigationBarItem(icon = {
-                    Icon(
-                        Icons.Default.Bookmark,
-                        contentDescription = stringResource(R.string.bookmarks_title)
-                    )
-                },
-                    label = { Text(stringResource(R.string.bookmarks_title)) },
-                    alwaysShowLabel = false,
-                    selected = selectedScreen == Screen.Bookmarks,
-                    onClick = {
-                        selectedScreen = Screen.Bookmarks
-                        navController.navigate(Screen.Bookmarks.route) {
-                            popUpTo(navController.graph.startDestinationId)
-                            launchSingleTop = true
-                        }
-                    })
-
-                NavigationBarItem(icon = {
-                    Icon(
-                        Icons.Default.Camera,
-                        contentDescription = stringResource(R.string.ocr_title)
-                    )
-                },
-                    label = { Text(stringResource(R.string.ocr_title)) },
-                    alwaysShowLabel = false,
-                    selected = selectedScreen == Screen.Ocr,
-                    onClick = {
-                        selectedScreen = Screen.Ocr
-                        navController.navigate(Screen.Ocr.route) {
-                            popUpTo(navController.graph.startDestinationId)
-                            launchSingleTop = true
-                        }
-                    })
-
-                NavigationBarItem(icon = {
-                    Icon(
-                        Icons.Default.Translate,
-                        contentDescription = stringResource(R.string.translate_title)
-                    )
-                },
-                    label = { Text(stringResource(R.string.translate_title)) },
-                    alwaysShowLabel = false,
-                    selected = selectedScreen == Screen.Translation,
-                    onClick = {
-                        selectedScreen = Screen.Translation
-                        navController.navigate(Screen.Translation.route) {
-                            popUpTo(navController.graph.startDestinationId)
-                            launchSingleTop = true
-                        }
-                    })
-            }
+        LingoLensBottomBar(navController, selectedScreen) { screen ->
+            selectedScreen = screen
         }
-    }) { innerPadding ->
+    }) { padding ->
         NavigationGraph(
             ocrViewModel = ocrViewModel,
             navController = navController,
-            modifier = Modifier.padding(innerPadding),
+            modifier = Modifier.padding(padding),
             startCamera = startCamera,
             imageSelector = imageSelector
         )
     }
+}
 
+private fun Screen.toContentType() = when (this) {
+    Screen.Translation -> ContentType.TRANSLATION
+    Screen.Ocr -> ContentType.OCR
+    else -> ContentType.TRANSLATION
 }
