faea08c970ae6a0c30ec7ff65b18d9f11b692e68
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2024-12-27 23:43:48 +0200

    feat: Define SpeechState sealed interface

---
+++ core/domain/src/main/java/com/venom/domain/model/SpeechState.kt
@@ -0,0 +1,10 @@
+package com.venom.domain.model
+
+sealed interface SpeechState {
+    data object Idle : SpeechState
+    data object Listening : SpeechState
+    data class Paused(val lastResult: String) : SpeechState
+    data class Partial(val text: String) : SpeechState
+    data class Result(val text: String) : SpeechState
+    data class Error(val error: SpeechError) : SpeechState
+}
