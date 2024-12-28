ef551b27b8764ddb58ec4c04b2332a2381772331
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2024-12-28 23:44:14 +0200

    refactor: Update permission handling with Accompanist
    This commit replaces the custom permission request logic with the accompanist-permissions library, providing a more streamlined and robust approach to handling microphone permission requests.
    
    Key changes:
    
    Removed the custom permission request logic and replaced with rememberPermissionState from accompanist
    
    Updated the permission request call to use the state method launchPermissionRequest
    
    Updated SpeechDialogContent to take in STTViewModel
    
    Replaced the state and callback parameters for SpeechDialogContent to use methods of the viewmodel.

--- core/ui/src/main/java/com/venom/ui/components/common/RequestMicrophonePermission.kt
+++ core/ui/src/main/java/com/venom/ui/components/common/RequestMicrophonePermission.kt
@@ -1,30 +1,23 @@
 package com.venom.ui.components.common
 
 import android.Manifest
-import androidx.activity.compose.rememberLauncherForActivityResult
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.remember
-import androidx.compose.ui.platform.LocalContext
-import com.venom.utils.PermissionUtils
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.rememberPermissionState
 
+@OptIn(ExperimentalPermissionsApi::class)
 @Composable
-fun RequestMicrophonePermission(onPermissionResult: (Boolean) -> Unit) {
-    val context = LocalContext.current
-    val permissionUtils = remember { PermissionUtils(context) }
-
-    val permissionLauncher = rememberLauncherForActivityResult(
-        contract = ActivityResultContracts.RequestPermission()
-    ) { isGranted: Boolean ->
+fun RequestMicrophonePermission(
+    onPermissionResult: (Boolean) -> Unit
+) {
+    val permissionState = rememberPermissionState(
+        permission = Manifest.permission.RECORD_AUDIO
+    ) { isGranted ->
         onPermissionResult(isGranted)
     }
 
     LaunchedEffect(Unit) {
-        if (!permissionUtils.isMicrophonePermissionGranted()) {
-            permissionLauncher.launch(Manifest.permission.RECORD_AUDIO)
-        } else {
-            onPermissionResult(true)
-        }
+        permissionState.launchPermissionRequest()
     }
 }
