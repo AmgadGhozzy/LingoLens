70b405f016c464b55b2d942b448f622d091e020a
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2024-12-28 23:29:39 +0200

    feat: Improve language bar UI
    This commit enhances the LanguageBar component by:
    
    Adding animation to language items for smooth transitions.
    
    Replacing the custom button with an IconButton for the swap languages action
    
    Adding surface for background
    
    Adding animation to the swap button rotation.
    
    Moving bottom sheet logic to inside the Surface scope

--- core/ui/src/main/java/com/venom/ui/components/bars/LanguageBar.kt
+++ core/ui/src/main/java/com/venom/ui/components/bars/LanguageBar.kt
@@ -1,13 +1,16 @@
 package com.venom.ui.components.bars
 
+
+import androidx.compose.animation.AnimatedContent
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.spring
-import androidx.compose.foundation.background
-import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -16,6 +19,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
@@ -23,8 +27,9 @@ import androidx.compose.ui.unit.dp
 import com.venom.domain.model.LANGUAGES_LIST
 import com.venom.domain.model.LanguageItem
 import com.venom.resources.R
-import com.venom.ui.components.buttons.CustomButton
 import com.venom.ui.components.items.LanguageItemView
+import com.venom.ui.screen.LanguageSelectorBottomSheet
+
 
 /**
  * A customizable top bar for language translation.
@@ -36,7 +41,11 @@ import com.venom.ui.components.items.LanguageItemView
  * @param showNativeNameHint Whether to show the native name of languages.
  * @param showFlag Whether to show flags for languages.
  * @param modifier Additional modifier for customization.
+ * @param flagSize The size of the flag icon.
+ * @param containerColor The color of the container.
+ * @param contentColor The color of the content.
  */
+
 @Composable
 fun LanguageBar(
     sourceLang: LanguageItem,
@@ -47,67 +56,93 @@ fun LanguageBar(
     showNativeNameHint: Boolean = false,
     showFlag: Boolean = false,
     modifier: Modifier = Modifier,
-    flagSize: Dp = 32.dp
+    flagSize: Dp = 32.dp,
+    containerColor: Color = MaterialTheme.colorScheme.surfaceContainerLow,
+    contentColor: Color = MaterialTheme.colorScheme.onSurface,
 ) {
-
     var isSelectingSourceLanguage by remember { mutableStateOf(true) }
     var showLanguageSelector by remember { mutableStateOf(false) }
 
-    // Animation for the rotation of the swap button
     val rotation by animateFloatAsState(
         targetValue = if (isFromBottomSheet) 0f else 180f,
-        animationSpec = spring(stiffness = Spring.StiffnessMediumLow),
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        ),
         label = "Swap Button Rotation"
     )
 
-    // Language selection bottom sheet
-//    if (showLanguageSelector && !isFromBottomSheet) {
-//        LanguageSelectorBottomSheet(initialSourceLang = sourceLang,
-//            initialTargetLang = targetLang,
-//            isSelectingSourceLanguage = isSelectingSourceLanguage,
-//            onLanguageSelected = { selectedLanguage ->
-//                onLanguageSelect(isSelectingSourceLanguage, selectedLanguage)
-//                showLanguageSelector = false
-//            },
-//            onDismiss = { showLanguageSelector = false })
-//    }
-
-    val interactionSource = remember { MutableInteractionSource() }
-    val backgroundColor = if (isSelectingSourceLanguage) {
-        MaterialTheme.colorScheme.primaryContainer.copy(alpha = 0.3f)
-    } else {
-        Color.Transparent
-    }
-    Row(
-        modifier = modifier
-            .fillMaxWidth()
-            .background(MaterialTheme.colorScheme.surfaceContainerLow)
-            .padding(8.dp),
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.SpaceEvenly
+    Surface(
+        modifier = modifier.fillMaxWidth(),
+        color = containerColor,
+        contentColor = contentColor,
+        tonalElevation = 1.dp
     ) {
-        // Source Language Selector
-        LanguageItemView(language = sourceLang, showFlag = showFlag, onClick = {
-            isSelectingSourceLanguage = true
-            showLanguageSelector = true
-        })
-        // Swap Button
-        CustomButton(
-            icon = R.drawable.icon_swap,
-            contentDescription = stringResource(id = R.string.action_swap_languages),
-            onClick = onSwapLanguages,
-            modifier = Modifier.rotate(rotation)
-        )
+        Row(
+            modifier = Modifier.padding(horizontal = 16.dp, vertical = 4.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            AnimatedContent(
+                targetState = sourceLang,
+                label = "Source Language Animation"
+            ) { language ->
+                LanguageItemView(
+                    language = language,
+                    showFlag = showFlag,
+                    flagSize = flagSize,
+                    showNativeNameHint = showNativeNameHint,
+                    onClick = {
+                        isSelectingSourceLanguage = true
+                        showLanguageSelector = true
+                    }
+                )
+            }
 
-        // Target Language Selector
-        LanguageItemView(language = targetLang, showFlag = showFlag, onClick = {
-            isSelectingSourceLanguage = true
-            showLanguageSelector = true
-        })
+            IconButton(
+                onClick = onSwapLanguages,
+                modifier = Modifier.rotate(rotation)
+            ) {
+                Icon(
+                    painter = painterResource(R.drawable.icon_swap),
+                    contentDescription = stringResource(R.string.action_swap_languages),
+                    tint = MaterialTheme.colorScheme.primary
+                )
+            }
+
+            AnimatedContent(
+                targetState = targetLang,
+                label = "Target Language Animation"
+            ) { language ->
+                LanguageItemView(
+                    language = language,
+                    showFlag = showFlag,
+                    flagSize = flagSize,
+                    showNativeNameHint = showNativeNameHint,
+                    onClick = {
+                        isSelectingSourceLanguage = false
+                        showLanguageSelector = true
+                    }
+                )
+            }
+        }
+    }
+
+    if (showLanguageSelector && !isFromBottomSheet) {
+        LanguageSelectorBottomSheet(
+            initialSourceLang = sourceLang,
+            initialTargetLang = targetLang,
+            isSelectingSourceLanguage = isSelectingSourceLanguage,
+            onLanguageSelected = { selectedLanguage ->
+                onLanguageSelect(isSelectingSourceLanguage, selectedLanguage)
+                showLanguageSelector = false
+            },
+            onDismiss = { showLanguageSelector = false }
+        )
     }
 }
 
-@Preview(showBackground = true, backgroundColor = 0xFF000000)
+@Preview
 @Composable
 fun TranslationTopBarPreview() {
     LanguageBar(sourceLang = LANGUAGES_LIST[0],
