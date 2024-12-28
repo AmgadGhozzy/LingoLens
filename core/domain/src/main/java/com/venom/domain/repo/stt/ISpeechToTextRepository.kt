b94102e79b50bc2f9cc8e6ff2290e1fdb0db2379
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2024-12-28 23:48:41 +0200

    feat: Remove cancelListening from ISpeechToTextRepository
    This commit removes the cancelListening method from the ISpeechToTextRepository interface. This is because the cancelListening operation was redundant as a stop listening operation is sufficient.
    
    Key changes:
    
    removed cancelListening from the interface
    
    removed import for SpeechConfig

--- core/domain/src/main/java/com/venom/domain/repo/stt/ISpeechToTextRepository.kt
+++ core/domain/src/main/java/com/venom/domain/repo/stt/ISpeechToTextRepository.kt
@@ -1,5 +1,6 @@
 package com.venom.domain.repo.stt
 
+import com.venom.domain.model.SpeechConfig
 import com.venom.domain.model.SpeechState
 import kotlinx.coroutines.flow.StateFlow
 
@@ -9,6 +10,5 @@ interface ISpeechToTextRepository {
     suspend fun startListening(config: SpeechConfig = SpeechConfig())
     suspend fun pauseListening()
     suspend fun stopListening()
-    suspend fun cancelListening()
     suspend fun destroy()
 }
