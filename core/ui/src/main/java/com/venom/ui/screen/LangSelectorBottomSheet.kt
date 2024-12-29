6b83103f7eb3a413dc935720f9a6797ad332aabe
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2024-12-29 23:51:42 +0200

    feat: Implement LanguageSelectorBottomSheet for language selection
    This commit introduces the LanguageSelectorBottomSheet, a composable for displaying a bottom sheet that allows users to select a language. The sheet includes a search bar to filter the language list, and the selected language is returned to the caller. This also includes a language bar at the top for showing selected languages and a list of languages.

--- core/ui/src/main/java/com/venom/ui/screen/LangSelectorBottomSheet.kt
+++ core/ui/src/main/java/com/venom/ui/screen/LangSelectorBottomSheet.kt
@@ -1,163 +1,50 @@
 package com.venom.ui.screen
 
-import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.venom.domain.model.LANGUAGES_LIST
-import com.venom.domain.model.LanguageItem
-import com.venom.resources.R
-import com.venom.ui.components.bars.LanguageBar
-import com.venom.ui.components.common.EmptyState
-import com.venom.ui.components.inputs.SearchBar
-import com.venom.ui.components.items.LanguageListItem
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.venom.ui.components.sections.LangSelectorContent
+import com.venom.ui.viewmodel.LangSelectorViewModel
 import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun LanguageSelectorBottomSheet(
-    languages: List<LanguageItem> = LANGUAGES_LIST,
-    initialSourceLang: LanguageItem,
-    initialTargetLang: LanguageItem,
-    isSelectingSourceLanguage: Boolean, // Add this parameter
-    onLanguageSelected: (LanguageItem) -> Unit,
+fun LangSelectorBottomSheet(
+    viewModel: LangSelectorViewModel = hiltViewModel(),
     onDismiss: () -> Unit
 ) {
-    var selectedLanguage by remember {
-        mutableStateOf(
-            if (isSelectingSourceLanguage) initialSourceLang
-            else initialTargetLang
-        )
-    }
-    val screenHeight = LocalConfiguration.current.screenHeightDp
-    val maxHeight = screenHeight * 0.9f
-    val offsetY = screenHeight * 0.1f
-
-    var searchQuery by remember { mutableStateOf("") }
+    val state by viewModel.state.collectAsState()
+    val scope = rememberCoroutineScope()
     val sheetState = rememberModalBottomSheetState(skipPartiallyExpanded = true)
-    val coroutineScope = rememberCoroutineScope()
+    val configuration = LocalConfiguration.current
+
+    val bottomSheetHeight = remember(configuration) {
+        configuration.screenHeightDp * 0.8f
+    }
 
 
-    var sourceLang by remember { mutableStateOf(initialSourceLang) }
-    var targetLang by remember { mutableStateOf(initialTargetLang) }
-
-    // Filter languages based on search query
-    val filteredLanguages = remember(searchQuery, languages) {
-        languages.filter {
-            searchQuery.isEmpty() || listOf(
-                it.code,
-                it.name,
-            ).any { name ->
-                name.contains(
-                    searchQuery, ignoreCase = true
-                )
-            }
-        }
+    LaunchedEffect(Unit) {
+        sheetState.show()
     }
 
     ModalBottomSheet(
-        onDismissRequest = {
-            coroutineScope.launch { sheetState.hide() }
-            onDismiss()
-        },
+        onDismissRequest = { scope.launch { sheetState.hide(); onDismiss() } },
         sheetState = sheetState,
         containerColor = MaterialTheme.colorScheme.surfaceContainerLow,
         modifier = Modifier
-            .fillMaxHeight(maxHeight)
+            .fillMaxHeight(bottomSheetHeight)
             .navigationBarsPadding()
-            .offset(y = offsetY.dp)
     ) {
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 16.dp)
-        ) {
-
-            LanguageBar(
-                sourceLang = sourceLang,
-                targetLang = targetLang,
-                isFromBottomSheet = true,
-                onSwapLanguages = {
-                    // Swap languages when swap button is pressed
-                    val temp = sourceLang
-                    sourceLang = targetLang
-                    targetLang = temp
-                },
-                onLanguageSelect = { isSource, _ ->
-                },
-                showNativeNameHint = false,
-                showFlag = true
-            )
-
-            // Search Bar
-            SearchBar(searchQuery = searchQuery, onSearchQueryChanged = { searchQuery = it })
-
-            // Language List Section
-            LanguageListSection(filteredLanguages = filteredLanguages,
-                selectedLanguage = selectedLanguage,
-                onLanguageSelected = { language ->
-                    sourceLang = language
-                    targetLang = language
-                    selectedLanguage = language
-                    onLanguageSelected(language)
-                    // Close the bottom sheet
-                    coroutineScope.launch {
-                        sheetState.hide()
-                        onDismiss()
-                    }
-
-                })
-        }
-    }
-}
-
-@Composable
-private fun LanguageListSection(
-    filteredLanguages: List<LanguageItem>,
-    selectedLanguage: LanguageItem?,
-    onLanguageSelected: (LanguageItem) -> Unit
-) {
-
-    LazyColumn(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(top = 16.dp)
-    ) {
-        if (filteredLanguages.isNotEmpty()) {
-            // Language list items
-            items(filteredLanguages, key = { it.code }) { language ->
-                LanguageListItem(language = language,
-                    isSelected = language == selectedLanguage,
-                    onClick = { onLanguageSelected(language) })
-            }
-        } else {
-            // Empty state item
-            item {
-                EmptyState(
-                    icon = R.drawable.icon_search,
-                    title = stringResource(R.string.search_no_results_title),
-                )
-            }
-        }
-    }
-}
-
-@Preview(showBackground = true, backgroundColor = 0xFF000000, uiMode = UI_MODE_NIGHT_YES)
-@Composable
-fun LanguageSelectorBottomSheetPreview() {
-    MaterialTheme {
-        LanguageSelectorBottomSheet(onDismiss = {},
-            onLanguageSelected = { _ -> },
-            initialSourceLang = LANGUAGES_LIST[0],
-            initialTargetLang = LANGUAGES_LIST[1],
-            isSelectingSourceLanguage = true
+        LangSelectorContent(
+            state = state,
+            onSearchQueryChange = viewModel::onSearchQueryChange,
+            onLanguageSelected = { language ->
+                viewModel.onLanguageSelected(language)
+                scope.launch { sheetState.hide(); onDismiss() }
+            },
         )
     }
 }
