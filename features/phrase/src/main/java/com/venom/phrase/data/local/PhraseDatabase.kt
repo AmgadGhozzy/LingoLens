43cfff559e416caa40dbc2ae6a017cc55926ca2e
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2024-12-30 23:59:27 +0200

    feat: Implement PhraseDatabase with Room entities and DAOs
    The PhraseDatabase has been implemented using Room. It defines the entities (Phrase, Category, and Section)

---
+++ features/phrase/src/main/java/com/venom/phrase/data/local/PhraseDatabase.kt
@@ -0,0 +1,17 @@
+package com.venom.phrase.data.local
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.venom.phrase.data.local.dao.CategoryDao
+import com.venom.phrase.data.local.dao.PhraseDao
+import com.venom.phrase.data.model.Category
+import com.venom.phrase.data.model.Phrase
+import com.venom.phrase.data.model.Section
+
+@Database(
+    entities = [Phrase::class, Category::class, Section::class], version = 1, exportSchema = false
+)
+abstract class PhraseDatabase : RoomDatabase() {
+    abstract fun phraseDao(): PhraseDao
+    abstract fun categoryDao(): CategoryDao
+}
