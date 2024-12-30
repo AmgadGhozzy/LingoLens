271356576ff31bf0633483cca2e0410d9c5090ca
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2024-12-30 23:51:14 +0200

    refactor: Move language section composables to ui package

--- core/ui/src/main/java/com/venom/ui/components/sections/ThesaurusView.kt
+++ core/ui/src/main/java/com/venom/ui/components/sections/ThesaurusView.kt
@@ -1,4 +1,4 @@
-package com.venom.lingopro.ui.components.sections
+package com.venom.ui.components.sections
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
