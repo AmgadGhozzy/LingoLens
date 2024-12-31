71e0a3c4c9884b64c84b41eeec155f312e97022b
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2024-12-31 21:35:48 +0200

    feat: Use Flow for PhraseDao
    Changed getPhrasesForSection to return a Flow of List<Phrase>.
    
    Changed ordering from priority to phrase_id.

--- features/phrase/src/main/java/com/venom/phrase/data/local/dao/PhraseDao.kt
+++ features/phrase/src/main/java/com/venom/phrase/data/local/dao/PhraseDao.kt
@@ -3,9 +3,10 @@ package com.venom.phrase.data.local.dao
 import androidx.room.Dao
 import androidx.room.Query
 import com.venom.phrase.data.model.Phrase
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface PhraseDao {
-    @Query("SELECT * FROM phrases WHERE section_id = :sectionId ORDER BY priority")
-    fun getPhrasesForSection(sectionId: Int): List<Phrase>
+    @Query("SELECT * FROM phrases WHERE section_id = :sectionId ORDER BY phrase_id")
+    fun getPhrasesForSection(sectionId: Int): Flow<List<Phrase>>
 }
