db8d6be4fc736d59df0079ebbf50d32bea6bcba4
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2024-12-31 21:39:40 +0200

    feat: Update PhraseRepository to use Flows and add SectionDao
    Updated getAllCategories to return a Flow.
    
    Removed getCategoryById.
    
    Added sectionDao dependency.
    
    Added getSectionsForCategory to fetch sections using sectionDao and returns a flow.
    
    Updated getPhrasesForSection to return a Flow.

--- features/phrase/src/main/java/com/venom/phrase/data/repo/PhraseRepository.kt
+++ features/phrase/src/main/java/com/venom/phrase/data/repo/PhraseRepository.kt
@@ -2,16 +2,23 @@ package com.venom.phrase.data.repo
 
 import com.venom.phrase.data.local.dao.CategoryDao
 import com.venom.phrase.data.local.dao.PhraseDao
+import com.venom.phrase.data.local.dao.SectionDao
 import com.venom.phrase.data.model.Category
 import com.venom.phrase.data.model.Phrase
+import com.venom.phrase.data.model.Section
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class PhraseRepository @Inject constructor(
-    private val phraseDao: PhraseDao, private val categoryDao: CategoryDao
+    private val phraseDao: PhraseDao,
+    private val categoryDao: CategoryDao,
+    private val sectionDao: SectionDao
 ) {
-    fun getPhrasesForSection(sectionId: Int): List<Phrase> =
-        phraseDao.getPhrasesForSection(sectionId)
+    fun getAllCategories(): Flow<List<Category>> = categoryDao.getAllCategories()
 
-    fun getAllCategories(): List<Category> = categoryDao.getAllCategories()
-    fun getCategoryById(categoryId: Int): Category = categoryDao.getCategoryById(categoryId)
+    fun getSectionsForCategory(categoryId: Int): Flow<List<Section>> =
+        sectionDao.getSectionsForCategory(categoryId)
+
+    fun getPhrasesForSection(sectionId: Int): Flow<List<Phrase>> =
+        phraseDao.getPhrasesForSection(sectionId)
 }
