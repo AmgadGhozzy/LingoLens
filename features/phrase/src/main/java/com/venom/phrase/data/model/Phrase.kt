ef2492e67bf9ed1806af26f874ae2e3b34cebf6c
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2024-12-31 21:38:14 +0200

    feat: Add multi-language support to Phrase
    Changed phrase_id to phraseId and made it nullable.
    
    Added default values to sectionId and priority.
    
    Added support for multiple language columns, each with a default value of an empty string.
    
    Renamed english to enUS.

--- features/phrase/src/main/java/com/venom/phrase/data/model/Phrase.kt
+++ features/phrase/src/main/java/com/venom/phrase/data/model/Phrase.kt
@@ -6,8 +6,142 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "phrases")
 data class Phrase(
-    @PrimaryKey val phrase_id: Int,
-    val section_id: Int,
+    @PrimaryKey
+    @ColumnInfo(name = "phrase_id")
+    val phraseId: Int?,
+
+    @ColumnInfo(name = "section_id", defaultValue = "0")
+    val sectionId: Int,
+
+    @ColumnInfo(name = "priority", defaultValue = "100")
     val priority: Int,
-    @ColumnInfo(name = "en-US") val english: String?
+
+    @ColumnInfo(name = "en-US", defaultValue = "''")
+    val enUS: String,
+
+    @ColumnInfo(name = "de", defaultValue = "''")
+    val de: String,
+
+    @ColumnInfo(name = "sw", defaultValue = "''")
+    val sw: String,
+
+    @ColumnInfo(name = "zh-CN", defaultValue = "''")
+    val zhCN: String,
+
+    @ColumnInfo(name = "da", defaultValue = "''")
+    val da: String,
+
+    @ColumnInfo(name = "cs", defaultValue = "''")
+    val cs: String,
+
+    @ColumnInfo(name = "en-AU", defaultValue = "''")
+    val enAU: String,
+
+    @ColumnInfo(name = "zh-TW", defaultValue = "''")
+    val zhTW: String,
+
+    @ColumnInfo(name = "ar-SA", defaultValue = "''")
+    val arSA: String,
+
+    @ColumnInfo(name = "fr-CA", defaultValue = "''")
+    val frCA: String,
+
+    @ColumnInfo(name = "es-US", defaultValue = "''")
+    val esUS: String,
+
+    @ColumnInfo(name = "fr-FR", defaultValue = "''")
+    val frFR: String,
+
+    @ColumnInfo(name = "en-UK", defaultValue = "''")
+    val enUK: String,
+
+    @ColumnInfo(name = "es-MX", defaultValue = "''")
+    val esMX: String,
+
+    @ColumnInfo(name = "es-ES", defaultValue = "''")
+    val esES: String,
+
+    @ColumnInfo(name = "hu", defaultValue = "''")
+    val hu: String,
+
+    @ColumnInfo(name = "uk", defaultValue = "''")
+    val uk: String,
+
+    @ColumnInfo(name = "ar-EG", defaultValue = "''")
+    val arEG: String,
+
+    @ColumnInfo(name = "zh-HK", defaultValue = "''")
+    val zhHK: String,
+
+    @ColumnInfo(name = "pt-PT", defaultValue = "''")
+    val ptPT: String,
+
+    @ColumnInfo(name = "ar-AE", defaultValue = "''")
+    val arAE: String,
+
+    @ColumnInfo(name = "pt-BR", defaultValue = "''")
+    val ptBR: String,
+
+    @ColumnInfo(name = "tr", defaultValue = "''")
+    val tr: String,
+
+    @ColumnInfo(name = "ja", defaultValue = "''")
+    val ja: String,
+
+    @ColumnInfo(name = "fi", defaultValue = "''")
+    val fi: String,
+
+    @ColumnInfo(name = "sk", defaultValue = "''")
+    val sk: String,
+
+    @ColumnInfo(name = "iw", defaultValue = "''")
+    val iw: String,
+
+    @ColumnInfo(name = "ms", defaultValue = "''")
+    val ms: String,
+
+    @ColumnInfo(name = "hr", defaultValue = "''")
+    val hr: String,
+
+    @ColumnInfo(name = "vi", defaultValue = "''")
+    val vi: String,
+
+    @ColumnInfo(name = "ca", defaultValue = "''")
+    val ca: String,
+
+    @ColumnInfo(name = "th", defaultValue = "''")
+    val th: String,
+
+    @ColumnInfo(name = "pl", defaultValue = "''")
+    val pl: String,
+
+    @ColumnInfo(name = "sv", defaultValue = "''")
+    val sv: String,
+
+    @ColumnInfo(name = "id", defaultValue = "''")
+    val id: String,
+
+    @ColumnInfo(name = "ro", defaultValue = "''")
+    val ro: String,
+
+    @ColumnInfo(name = "nl", defaultValue = "''")
+    val nl: String,
+
+    @ColumnInfo(name = "ko", defaultValue = "''")
+    val ko: String,
+
+    @ColumnInfo(name = "el", defaultValue = "''")
+    val el: String,
+
+    @ColumnInfo(name = "it", defaultValue = "''")
+    val it: String,
+
+    @ColumnInfo(name = "no", defaultValue = "''")
+    val no: String,
+
+    @ColumnInfo(name = "hi", defaultValue = "''")
+    val hi: String,
+
+    @ColumnInfo(name = "ru", defaultValue = "''")
+    val ru: String
 )
