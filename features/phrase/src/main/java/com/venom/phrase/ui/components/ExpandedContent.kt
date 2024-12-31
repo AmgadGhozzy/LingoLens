d156d74d00b0ea8ac8336e4d916b2924923a5905
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2024-12-31 22:30:11 +0200

    feat: Create ExpandedContent component

---
+++ features/phrase/src/main/java/com/venom/phrase/ui/components/ExpandedContent.kt
@@ -0,0 +1,45 @@
+package com.venom.phrase.ui.components
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.shrinkVertically
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.venom.phrase.data.model.Phrase
+
+
+@Composable
+fun ExpandedContent(
+    visible: Boolean, phrase: Phrase
+) {
+    AnimatedVisibility(
+        visible = visible,
+        enter = expandVertically() + fadeIn(),
+        exit = shrinkVertically() + fadeOut()
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 16.dp)
+        ) {
+            HorizontalDivider(
+                modifier = Modifier.padding(bottom = 16.dp),
+                color = MaterialTheme.colorScheme.outlineVariant
+            )
+
+            TranslatedText(phrase.arEG)
+            Spacer(modifier = Modifier.height(8.dp))
+            ActionButtons()
+        }
+    }
+}
