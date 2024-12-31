cae8ea02179c1197bca59504561e2b8aab2f78ca
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2024-12-31 21:35:25 +0200

    fix: Use Flow for CategoryDao and remove getCategoryById
    Changed getAllCategories to return a Flow of List<Category>.
    
    Removed getCategoryById query.

--- features/phrase/src/main/java/com/venom/phrase/data/local/dao/CategoryDao.kt
+++ features/phrase/src/main/java/com/venom/phrase/data/local/dao/CategoryDao.kt
@@ -1,14 +1,12 @@
 package com.venom.phrase.data.local.dao
 
-import android.icu.util.ULocale.Category
 import androidx.room.Dao
 import androidx.room.Query
+import com.venom.phrase.data.model.Category
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface CategoryDao {
-    @Query("SELECT * FROM categories ORDER BY priority")
-    fun getAllCategories(): List<Category>
-
-    @Query("SELECT * FROM categories WHERE category_id = :categoryId")
-    fun getCategoryById(categoryId: Int): Category
+    @Query("SELECT * FROM categories ORDER BY category_id")
+    fun getAllCategories(): Flow<List<Category>>
 }
