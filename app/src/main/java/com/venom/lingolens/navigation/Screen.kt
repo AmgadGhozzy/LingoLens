5b6c36b93163c378c2728367c5ae1e687fd52982
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2024-12-31 21:29:20 +0200

    feat: Add category route to Phrases screen

--- app/src/main/java/com/venom/lingolens/navigation/Screen.kt
+++ app/src/main/java/com/venom/lingolens/navigation/Screen.kt
@@ -4,6 +4,9 @@ sealed class Screen(val route: String) {
     object Translation : Screen("translation")
     object Ocr : Screen("ocr")
     object Dictionary : Screen("dictionary")
+    object PhrasesCategory : Screen("category/{categoryId}") {
+        fun createRoute(categoryId: Int) = "category/$categoryId"
+    }
     object Phrases : Screen("phrases")
 
 
