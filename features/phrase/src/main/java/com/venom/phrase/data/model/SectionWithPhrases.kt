9125d97f8e737c67ac7f447382fe2b192bfbc200
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2025-01-01 19:29:54 +0200

    feat: Add SectionWithPhrases data class
    The code added a new data class SectionWithPhrases to represent a section and its associated phrases.

---
+++ features/phrase/src/main/java/com/venom/phrase/data/model/SectionWithPhrases.kt
@@ -0,0 +1,10 @@
+package com.venom.phrase.data.model
+
+import androidx.room.Embedded
+import androidx.room.Relation
+
+data class SectionWithPhrases(
+    @Embedded val section: Section, @Relation(
+        parentColumn = "sectionId", entityColumn = "sectionId"
+    ) val phrases: List<Phrase>
+)
