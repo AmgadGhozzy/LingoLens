a11d8d7ba44a19e4a0da414d1cf13e24473c7189
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2025-01-01 19:27:43 +0200

    Refactor: Change PhraseDao to fetch categories and sections with phrases
    The code changed the PhraseDao to fetch categories and sections with their associated phrases, and removed the old getPhrasesForSection function.

--- features/phrase/src/main/java/com/venom/phrase/data/local/dao/PhraseDao.kt
+++ features/phrase/src/main/java/com/venom/phrase/data/local/dao/PhraseDao.kt
@@ -2,11 +2,17 @@ package com.venom.phrase.data.local.dao
 
 import androidx.room.Dao
 import androidx.room.Query
-import com.venom.phrase.data.model.Phrase
-import kotlinx.coroutines.flow.Flow
+import androidx.room.Transaction
+import com.venom.phrase.data.model.Category
+import com.venom.phrase.data.model.SectionWithPhrases
 
 @Dao
 interface PhraseDao {
-    @Query("SELECT * FROM phrases WHERE section_id = :sectionId ORDER BY phrase_id")
-    fun getPhrasesForSection(sectionId: Int): Flow<List<Phrase>>
+
+    @Query("SELECT * FROM categories")
+    fun getAllCategories(): List<Category>
+
+    @Transaction
+    @Query("SELECT * FROM SECTIONS WHERE categoryId = :categoryId")
+    fun getSectionsWithPhrases(categoryId: Int): List<SectionWithPhrases>
 }
