dd207fe325dcb80b4f08ff7d5634fc744eeac9c5
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2025-01-01 19:30:41 +0200

    Refactor: Update DI module to use new dao and repository
    The code updated the DI module to provide the new PhraseDao and PhraseRepository and removed the unused category and section daos.

--- features/phrase/src/main/java/com/venom/phrase/di/PhraseModule.kt
+++ features/phrase/src/main/java/com/venom/phrase/di/PhraseModule.kt
@@ -3,9 +3,7 @@ package com.venom.phrase.di
 import android.content.Context
 import androidx.room.Room
 import com.venom.phrase.data.local.PhraseDatabase
-import com.venom.phrase.data.local.dao.CategoryDao
 import com.venom.phrase.data.local.dao.PhraseDao
-import com.venom.phrase.data.local.dao.SectionDao
 import com.venom.phrase.data.repo.PhraseRepository
 import dagger.Module
 import dagger.Provides
@@ -22,28 +20,18 @@ object AppModule {
     @Singleton
     fun provideDatabase(@ApplicationContext context: Context): PhraseDatabase {
         return Room.databaseBuilder(
-            context, PhraseDatabase::class.java, "Phrasebook.db"
-        ).createFromAsset("Phrasebook.db").build()
+            context, PhraseDatabase::class.java, "phrases.db"
+        ).createFromAsset("phrases.db").build()
     }
 
     @Provides
     @Singleton
-    fun provideCategoryDao(database: PhraseDatabase) = database.categoryDao()
-
-    @Provides
-    @Singleton
-    fun provideSectionDao(database: PhraseDatabase) = database.sectionDao()
-
-
-    @Provides
-    @Singleton
-    fun providePhraseDao(database: PhraseDatabase) = database.phraseDao()
-
+    fun providePhaseDao(database: PhraseDatabase) = database.phraseDao()
 
     @Provides
     @Singleton
     fun provideRepository(
-        phraseDao: PhraseDao, categoryDao: CategoryDao, sectionDao: SectionDao
-    ) = PhraseRepository(phraseDao, categoryDao, sectionDao)
+        phraseDao: PhraseDao
+    ) = PhraseRepository(phraseDao)
 }
 
