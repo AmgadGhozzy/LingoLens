69bd28198e78e42036335c4986d867d609bc43b1
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2025-01-01 03:25:17 +0200

    Fix: Move search bar and adjust language bar
    The search bar was removed, and the language bar is now aligned to the end with a fixed width.

--- features/phrase/src/main/java/com/venom/phrase/ui/components/PhrasebookContent.kt
+++ features/phrase/src/main/java/com/venom/phrase/ui/components/PhrasebookContent.kt
@@ -7,12 +7,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.input.nestedscroll.nestedScroll
@@ -44,27 +46,17 @@ fun PhrasebookContent(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(padding)
-                .systemBarsPadding()
                 .navigationBarsPadding(), verticalArrangement = Arrangement.spacedBy(8.dp)
         ) {
             LanguageBar(
                 viewModel = langSelectorViewModel,
                 modifier = Modifier
-                    .padding(horizontal = 16.dp)
+                    .align(Alignment.End).width(256.dp)
                     .clip(RoundedCornerShape(28.dp))
-                    .border(
-                        1.dp, MaterialTheme.colorScheme.outlineVariant, RoundedCornerShape(28.dp)
-                    )
+                    .border(0.5.dp, MaterialTheme.colorScheme.outlineVariant, RoundedCornerShape(28.dp))
             )
 
-            CustomSearchBar(
-                searchQuery = searchQuery,
-                onSearchQueryChanged = onSearchQueryChange,
-            )
-
-            CategoryList(
-                categories = state.categories, onCategoryClick = onNavigateToCategory
-            )
+            CategoryList(state = state, onCategoryClick = onNavigateToCategory)
         }
     }
 }
