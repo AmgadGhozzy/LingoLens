81338559bd8da864e5400dc51d15b44b0298f7d3
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2025-01-01 19:27:14 +0200

    Fix: Remove unused CategoryDao and sectionDao
    The code removed the unused categoryDao and sectionDao functions and set the exportSchema to true

--- features/phrase/src/main/java/com/venom/phrase/data/local/PhraseDatabase.kt
+++ features/phrase/src/main/java/com/venom/phrase/data/local/PhraseDatabase.kt
@@ -2,16 +2,14 @@ package com.venom.phrase.data.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import com.venom.phrase.data.local.dao.CategoryDao
 import com.venom.phrase.data.local.dao.PhraseDao
 import com.venom.phrase.data.model.Category
 import com.venom.phrase.data.model.Phrase
 import com.venom.phrase.data.model.Section
 
-@Database(entities = [Phrase::class, Category::class, Section::class], version = 1, exportSchema = false)
+@Database(
+    entities = [Phrase::class, Category::class, Section::class], version = 1, exportSchema = true
+)
 abstract class PhraseDatabase : RoomDatabase() {
-    abstract fun categoryDao(): CategoryDao
-    abstract fun sectionDao(): CategoryDao
     abstract fun phraseDao(): PhraseDao
-
 }
