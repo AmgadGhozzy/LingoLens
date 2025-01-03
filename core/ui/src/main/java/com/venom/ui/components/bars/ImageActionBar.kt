a9c7017a2540421798bf021a383efaac1e197f19
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2025-01-03 17:37:05 +0200

    Refactor: Use BaseActionBar for ImageActionBar
    The ImageActionBar composable is refactored to use a new BaseActionBar composable, reducing code duplication and improving maintainability.

--- core/ui/src/main/java/com/venom/ui/components/bars/ImageActionBar.kt
+++ core/ui/src/main/java/com/venom/ui/components/bars/ImageActionBar.kt
@@ -1,51 +1,23 @@
 package com.venom.ui.components.bars
 
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.dp
 import com.venom.resources.R
-import com.venom.ui.components.buttons.CustomButton
-import com.venom.ui.components.dialogs.CustomCard
-import com.venom.ui.components.menus.DropdownMenuTrigger
+import com.venom.ui.components.common.ActionItem
+import com.venom.ui.components.common.BaseActionBar
 
 @Composable
 fun ImageActionBar(
     actions: List<ActionItem.Action>,
-    modifier: Modifier = Modifier,
+    modifier: Modifier = Modifier
 ) {
-    CustomCard (
-        modifier = modifier
-            .fillMaxWidth()
-            .wrapContentHeight()
-            .padding(horizontal = 6.dp)
-    ){
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(vertical = 4.dp)
-                .padding(end = 18.dp),
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically
-        ){
-            DropdownMenuTrigger(onOptionSelected = { it.onClick() })
-            actions.forEach { action ->
-                CustomButton(
-                    icon = action.icon,
-                    contentDescription = stringResource(action.textRes),
-                    onClick = action.onClick,
-                    enabled = action.enabled,
-                    selected = action.selected
-                )
-            }
-        }
-    }
+    BaseActionBar(
+        actions = actions,
+        containerColor = MaterialTheme.colorScheme.secondaryContainer,
+        showDropdownMenu = true, useCard = true, modifier = modifier
+    )
 }
 
 @Preview(showBackground = true)
