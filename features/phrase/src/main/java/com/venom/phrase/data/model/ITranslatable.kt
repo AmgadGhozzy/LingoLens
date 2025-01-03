d6266337a42bd841fb31cd1d0ed485df6f730147
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2025-01-03 18:22:35 +0200

    feat: Add ITranslatable Interface definition

---
+++ features/phrase/src/main/java/com/venom/phrase/data/model/ITranslatable.kt
@@ -0,0 +1,36 @@
+package com.venom.phrase.data.model
+
+interface ITranslatable {
+    val englishEn: String
+    val arabicAr: String
+    val frenchFr: String
+    val germanDe: String
+    val spanishEs: String
+    val chineseZh: String
+    val portuguesePt: String
+    val swahiliSw: String
+    val czechCs: String
+    val hungarianHu: String
+    val ukrainianUk: String
+    val turkishTr: String
+    val japaneseJa: String
+    val finnishFi: String
+    val slovakSk: String
+    val hebrewHe: String
+    val malaysMs: String
+    val croatianHr: String
+    val vietnameseVi: String
+    val catalanCa: String
+    val thaiTh: String
+    val polishPl: String
+    val swedishSv: String
+    val indonesianId: String
+    val romanianRo: String
+    val dutchNl: String
+    val koreanKo: String
+    val greekEl: String
+    val italianIt: String
+    val norwegianNo: String
+    val hindiHi: String
+    val russianRu: String
+}
