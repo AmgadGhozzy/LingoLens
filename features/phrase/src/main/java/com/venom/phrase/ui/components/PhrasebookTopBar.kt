890b5e7ef88fb848efe21cff52a3f4131fc220a9
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2025-01-04 20:51:24 +0200

    refactor: Move language selector to top bar
    This commit moves the language selector from the main content to the top app bar, and removes the search bar from the top app bar.

--- features/phrase/src/main/java/com/venom/phrase/ui/components/PhrasebookTopBar.kt
+++ features/phrase/src/main/java/com/venom/phrase/ui/components/PhrasebookTopBar.kt
@@ -1,9 +1,11 @@
 package com.venom.phrase.ui.components
 
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Badge
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -12,32 +14,38 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.venom.resources.R
-import com.venom.ui.components.inputs.CustomSearchBar
+import com.venom.ui.components.bars.LanguageBar
+import com.venom.ui.viewmodel.LangSelectorViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun PhrasebookTopBar(
+    viewModel: LangSelectorViewModel,
     totalPhrases: Int,
     scrollBehavior: TopAppBarScrollBehavior,
-    searchQuery: String,
-    onSearchQueryChange: (String) -> Unit
 ) {
     TopAppBar(
         modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
         scrollBehavior = scrollBehavior,
         title = { PhrasesAvailableRow(totalPhrases) },
         actions = {
-            CustomSearchBar(
-                modifier = Modifier.width(224.dp),
-                searchQuery = searchQuery,
-                onSearchQueryChanged = onSearchQueryChange,
+            LanguageBar(
+                viewModel = viewModel,
+                modifier = Modifier
+                    .width(224.dp)
+                    .clip(RoundedCornerShape(28.dp))
+                    .border(
+                        0.5.dp,
+                        MaterialTheme.colorScheme.outlineVariant,
+                        RoundedCornerShape(28.dp)
+                    )
             )
         },
         colors = TopAppBarDefaults.topAppBarColors(
