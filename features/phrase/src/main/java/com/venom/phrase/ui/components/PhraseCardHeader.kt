6f28055f5fd0fe47a52afccc371e9eb27e8ebab2
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2025-01-04 20:49:41 +0200

    feat: Use translations in PhraseCardHeader
    This commit modifies the PhraseCardHeader composable to display the phrase based on the selected source language.

--- features/phrase/src/main/java/com/venom/phrase/ui/components/PhraseCardHeader.kt
+++ features/phrase/src/main/java/com/venom/phrase/ui/components/PhraseCardHeader.kt
@@ -9,11 +9,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import com.venom.phrase.data.mapper.getTranslation
 import com.venom.phrase.data.model.Phrase
 
 @Composable
 fun PhraseCardHeader(
-    phrase: Phrase, isFavorite: Boolean, onFavoriteClick: () -> Unit
+    phrase: Phrase, sourceLang: String, isFavorite: Boolean, onFavoriteClick: () -> Unit
 ) {
     Row(
         modifier = Modifier.fillMaxWidth(),
@@ -21,9 +22,11 @@ fun PhraseCardHeader(
         verticalAlignment = Alignment.CenterVertically
     ) {
         Text(
-            text = phrase.enUS, style = MaterialTheme.typography.titleMedium.copy(
+            text = phrase.getTranslation(sourceLang),
+            style = MaterialTheme.typography.titleMedium.copy(
                 fontWeight = FontWeight.SemiBold
-            ), modifier = Modifier.weight(1f)
+            ),
+            modifier = Modifier.weight(1f)
         )
 
         ExpandCardActions(
