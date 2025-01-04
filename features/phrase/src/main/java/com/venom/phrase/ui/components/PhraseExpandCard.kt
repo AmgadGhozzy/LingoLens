5f0503ef94c175ee7750f4584474c396543bd092
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2025-01-04 20:20:16 +0200

    feat: Add expand/collapse button to PhraseCard
    This commit adds an expand/collapse button to the PhraseCard. It also passes source and target language
    to the content

--- features/phrase/src/main/java/com/venom/phrase/ui/components/PhraseExpandCard.kt
+++ features/phrase/src/main/java/com/venom/phrase/ui/components/PhraseExpandCard.kt
@@ -2,6 +2,9 @@ package com.venom.phrase.ui.components
 
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.ExpandLess
+import androidx.compose.material.icons.rounded.ExpandMore
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -11,22 +14,26 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.unit.dp
 import com.venom.phrase.data.model.Phrase
+import com.venom.resources.R
+import com.venom.ui.components.buttons.CustomButton
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun PhraseExpandCard(phrase: Phrase) {
+fun PhraseExpandCard(phrase: Phrase, sourceLang: String, targetLang: String) {
     var expanded by remember { mutableStateOf(false) }
     var isFavorite by remember { mutableStateOf(false) }
 
     Card(
         modifier = Modifier
             .fillMaxWidth()
-            .semantics { contentDescription = "Phrase card for ${phrase.enUS}" },
+            .semantics { contentDescription = "Phrase card for ${phrase.englishEn}" },
         shape = RoundedCornerShape(16.dp),
         onClick = { expanded = !expanded },
         colors = CardDefaults.cardColors(
@@ -40,6 +47,20 @@ fun PhraseExpandCard(phrase: Phrase) {
         PhraseCardContent(phrase = phrase,
             expanded = expanded,
             isFavorite = isFavorite,
+            sourceLang = sourceLang,
+            targetLang = targetLang,
             onFavoriteClick = { isFavorite = !isFavorite })
+
+        CustomButton(
+            icon = if (expanded) Icons.Rounded.ExpandLess else Icons.Rounded.ExpandMore,
+            onClick = { expanded = !expanded },
+            contentDescription = stringResource(
+                if (expanded) R.string.action_collapse
+                else R.string.action_expand
+            ),
+            modifier = Modifier
+                .align(Alignment.End)
+                .fillMaxWidth()
+        )
     }
 }
