32a6231bed6a8abbfa1f317712a0f76ecd89f97d
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2025-01-04 20:51:45 +0200

    feat: Make translated text selectable
    This commit modifies the TranslatedText composable to make the text selectable.

--- features/phrase/src/main/java/com/venom/phrase/ui/components/TranslatedText.kt
+++ features/phrase/src/main/java/com/venom/phrase/ui/components/TranslatedText.kt
@@ -1,25 +1,25 @@
 package com.venom.phrase.ui.components
 
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextDirection
 
 @Composable
 fun TranslatedText(text: String) {
-    Text(
-        text = text,
-        modifier = Modifier
-            .fillMaxWidth(),
-        style = TextStyle(
-            fontSize = MaterialTheme.typography.bodyLarge.fontSize,
-            color = MaterialTheme.colorScheme.onSurfaceVariant,
-            textDirection = TextDirection.Rtl
+    SelectionContainer {
+        Text(
+            text = text,
+            modifier = Modifier.fillMaxWidth(),
+            style = TextStyle(
+                fontSize = MaterialTheme.typography.titleSmall.fontSize,
+                color = MaterialTheme.colorScheme.onSurfaceVariant,
+                textDirection = TextDirection.Rtl
+            )
         )
-    )
+    }
 }
