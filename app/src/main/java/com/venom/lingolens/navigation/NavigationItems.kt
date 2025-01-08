b1df91198b35b97f2ead2d1108479c0179262cc5
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2025-01-08 17:10:19 +0200

    feat: Update NavigationItems
    Adds WordCard to the NavigationItems
    
    Updates icons and string resources for the items

--- app/src/main/java/com/venom/lingolens/navigation/NavigationItems.kt
+++ app/src/main/java/com/venom/lingolens/navigation/NavigationItems.kt
@@ -1,13 +1,15 @@
 package com.venom.lingolens.navigation
 
 import com.venom.resources.R
+
 enum class NavigationItems(
     val screen: Screen,
     val icon: Int,
     val titleRes: Int
 ) {
+    WORDCARD(Screen.WordCard, R.drawable.icon_cards1, R.string.wordcard_title),
     DIALOG(Screen.Dialog, R.drawable.icon_dialog, R.string.dialog_title),
-    BOOKMARKS(Screen.Phrases, R.drawable.icon_dialog, R.string.phrase_title),
+    PHRASE(Screen.Phrases, R.drawable.icon_dialog, R.string.phrase_title),
     OCR(Screen.Ocr, R.drawable.icon_camera, R.string.ocr_title),
     TRANSLATE(Screen.Translation, R.drawable.icon_translate, R.string.translate_title)
 }
