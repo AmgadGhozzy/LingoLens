8ef8ffd8613ee45ae5262011405ba995d48e7cb5
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2025-01-08 17:04:48 +0200

    feat: Update ActionButton UI
    Uses IconButton instead of Box with clickable modifier
    
    Streamlines the code by removing unnecessary Box container

--- features/wordcard/src/main/java/com/venom/wordcard/ui/components/ActionButton.kt
+++ features/wordcard/src/main/java/com/venom/wordcard/ui/components/ActionButton.kt
@@ -3,10 +3,13 @@ package com.venom.wordcard.ui.components
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -23,13 +26,12 @@ fun ActionButton(
     iconTint: Color,
     onClick: () -> Unit = {}
 ) {
-    Box(
+    IconButton(
+        onClick = onClick,
         modifier = Modifier
             .size(48.dp)
             .clip(CircleShape)
-            .background(backgroundColor.copy(alpha = 0.9f))
-            .clickable(onClick = onClick)
-            .padding(8.dp), contentAlignment = Alignment.Center
+            .background(backgroundColor.copy(alpha = 0.9f)),
     ) {
         Icon(
             imageVector = icon,
