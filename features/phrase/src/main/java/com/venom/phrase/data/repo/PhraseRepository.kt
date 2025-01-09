84dfdd6a35bba134afb6b69549faf3846b38f614
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2025-01-10 00:01:32 +0200

    feat: Update PhraseRepository
    This commit updates the PhraseRepository to include more functionalities related to the phrases management for card swiper and adds dispatcher to coroutines.

--- features/phrase/src/main/java/com/venom/phrase/data/repo/PhraseRepository.kt
+++ features/phrase/src/main/java/com/venom/phrase/data/repo/PhraseRepository.kt
@@ -1,12 +1,38 @@
 package com.venom.phrase.data.repo
 
 import com.venom.phrase.data.local.dao.PhraseDao
+import com.venom.phrase.data.model.Phrase
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.withContext
 
+
 class PhraseRepository(private val dao: PhraseDao) {
-    suspend fun getAllCategories() = withContext(Dispatchers.IO) { dao.getAllCategories() }
+    suspend fun getAllCategories() = withContext(Dispatchers.IO) {
+        dao.getAllCategories()
+    }
+
     suspend fun getSectionsWithPhrases(categoryId: Int) = withContext(Dispatchers.IO) {
         dao.getSectionsWithPhrases(categoryId)
+    }
+
+    fun get10UnseenPhrases(): Flow<List<Phrase>> = dao.get10UnseenPhrases()
+
+    fun getBookmarkedPhrases(): Flow<List<Phrase>> = dao.getBookmarkedPhrases()
+
+    fun getRememberedPhrases(): Flow<List<Phrase>> = dao.getRememberedPhrases()
+
+    fun getForgottenPhrases(): Flow<List<Phrase>> = dao.getForgotedPhrases()
+
+    suspend fun toggleBookmark(phrase: Phrase) = withContext(Dispatchers.IO) {
+        dao.update(phrase.copy(isBookmarked = !phrase.isBookmarked))
+    }
+
+    suspend fun toggleRemember(phrase: Phrase) = withContext(Dispatchers.IO) {
+        dao.update(phrase.copy(isRemembered = !phrase.isRemembered))
+    }
+
+    suspend fun toggleForgot(phrase: Phrase) = withContext(Dispatchers.IO) {
+        dao.update(phrase.copy(isForgotten = !phrase.isForgotten))
     }
 }
