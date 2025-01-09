3a8495212eb0aa45d4dea1cb40b7ec1d656bc4f4
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2025-01-09 23:59:39 +0200

    feat: Update PhraseDao
    This commit updates the PhraseDao to include more functionalities related to the phrases management for card swiper.

--- features/phrase/src/main/java/com/venom/phrase/data/local/dao/PhraseDao.kt
+++ features/phrase/src/main/java/com/venom/phrase/data/local/dao/PhraseDao.kt
@@ -3,16 +3,33 @@ package com.venom.phrase.data.local.dao
 import androidx.room.Dao
 import androidx.room.Query
 import androidx.room.Transaction
+import androidx.room.Update
 import com.venom.phrase.data.model.Category
+import com.venom.phrase.data.model.Phrase
 import com.venom.phrase.data.model.SectionWithPhrases
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface PhraseDao {
-
     @Query("SELECT * FROM categories")
     fun getAllCategories(): List<Category>
 
     @Transaction
     @Query("SELECT * FROM SECTIONS WHERE categoryId = :categoryId")
     fun getSectionsWithPhrases(categoryId: Int): List<SectionWithPhrases>
+
+    @Query("SELECT * FROM phrases WHERE isRemembered = 0 AND isForgotten = 0 ORDER BY sectionId LIMIT 10")
+    fun get10UnseenPhrases(): Flow<List<Phrase>>
+
+    @Query("SELECT * FROM phrases WHERE isBookmarked = 1")
+    fun getBookmarkedPhrases(): Flow<List<Phrase>>
+
+    @Query("SELECT * FROM phrases WHERE isRemembered = 1")
+    fun getRememberedPhrases(): Flow<List<Phrase>>
+
+    @Query("SELECT * FROM phrases WHERE isForgotten = 1")
+    fun getForgotedPhrases(): Flow<List<Phrase>>
+
+    @Update
+    suspend fun update(phrase: Phrase)
 }
