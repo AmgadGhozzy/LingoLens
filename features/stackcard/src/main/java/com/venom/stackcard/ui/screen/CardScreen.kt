253c84fd68d2f7557c18978a5815875f2b1825af
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2025-01-09 23:47:38 +0200

    feat: Integrate CardSwiperStack and create shared CardScreen
    This commit integrates the CardSwiperStack into a new CardScreen and WordCardScreen, that manages both word and phrase cards, and uses the same UI for the swiping effect by passing specific view models.

---
+++ features/stackcard/src/main/java/com/venom/stackcard/ui/screen/CardScreen.kt
@@ -0,0 +1,81 @@
+package com.venom.stackcard.ui.screen
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.venom.resources.R
+import com.venom.stackcard.ui.viewmodel.CardItem
+import com.venom.stackcard.ui.viewmodel.CardSwiperEvent
+import com.venom.stackcard.ui.viewmodel.CardSwiperEvent.SetCardType
+import com.venom.stackcard.ui.viewmodel.CardSwiperViewModel
+import com.venom.stackcard.ui.viewmodel.CardType
+import com.venom.ui.components.sections.CustomTabs
+import com.venom.ui.components.sections.TabItem
+import com.venom.ui.viewmodel.TTSViewModel
+import com.venom.utils.Extensions.copyToClipboard
+
+@Composable
+fun CardScreen(
+    wordViewModel: CardSwiperViewModel = hiltViewModel(),
+    phraseViewModel: CardSwiperViewModel = hiltViewModel(),
+    ttsViewModel: TTSViewModel = hiltViewModel(),
+) {
+    val context = LocalContext.current
+
+    // Actions for copy and speak functionalities
+    val copyAction: (String) -> Unit = { text -> context.copyToClipboard(text) }
+    val speakAction: (String) -> Unit = { text -> ttsViewModel.speak(text) }
+
+    var selectedTab by remember { mutableIntStateOf(0) }
+
+    // Determine the current view model based on the selected tab
+    val currentViewModel = if (selectedTab == 0) wordViewModel else phraseViewModel
+
+    // Actions for card events using the current view model
+
+    val onBookmarkWord: (CardItem) -> Unit =
+        { currentViewModel.onEvent(CardSwiperEvent.Bookmark(it)) }
+    val onRememberWord: (CardItem) -> Unit =
+        { currentViewModel.onEvent(CardSwiperEvent.Remember(it)) }
+    val onForgotWord: (CardItem) -> Unit = { currentViewModel.onEvent(CardSwiperEvent.Forgot(it)) }
+
+    // Set initial CardType for each ViewModel
+    LaunchedEffect(selectedTab) {
+        if (selectedTab == 0) wordViewModel.onEvent(SetCardType(CardType.WORD))
+        else wordViewModel.onEvent(SetCardType(CardType.PHRASE))
+    }
+
+    // Tabs for switching between card types
+    CustomTabs(
+        tabs = listOf(
+            TabItem(R.string.words_title, R.drawable.icon_cards1),
+            TabItem(R.string.phrase_title, R.drawable.icon_dialog)
+        ), selectedTab = selectedTab, onTabSelected = { newTab ->
+            selectedTab = newTab
+            currentViewModel.onEvent(SetCardType(if (newTab == 0) CardType.WORD else CardType.PHRASE))
+        }, modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 12.dp)
+    )
+
+    // Main content of the screen
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 124.dp)
+            .padding(48.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        CardSwiperStack(viewModel = currentViewModel,
+            onRememberCard = onRememberWord,
+            onForgotCard = onForgotWord,
+            onBookmarkCard = onBookmarkWord,
+            onSpeak = speakAction,
+            onCopy = copyAction,
+            onUndoLastAction = { })
+    }
+}
