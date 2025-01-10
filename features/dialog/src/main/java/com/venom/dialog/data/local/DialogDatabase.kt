5d4548e0d53101ca572d4e9b85854613c43ede45
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2025-01-10 19:17:40 +0200

    feat: Implement DialogDatabase using Room

---
+++ features/dialog/src/main/java/com/venom/dialog/data/local/DialogDatabase.kt
@@ -0,0 +1,11 @@
+package com.venom.dialog.data.local
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.venom.dialog.data.local.dao.DialogMessageDao
+import com.venom.dialog.data.local.model.DialogMessage
+
+@Database(entities = [DialogMessage::class], version = 1)
+abstract class DialogDatabase : RoomDatabase() {
+    abstract fun dialogMessageDao(): DialogMessageDao
+}
