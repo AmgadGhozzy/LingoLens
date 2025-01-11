a8ba405197766a684bafee9fc49aee621a6ae89b
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2025-01-11 23:23:50 +0200

    Refactor: Improve DynamicStyledText component
    Improved DynamicStyledText with customizable shadow, text direction, and line height.

---
+++ core/ui/src/main/java/com/venom/ui/components/common/DynamicStyledText.kt
@@ -0,0 +1,51 @@
+package com.venom.ui.components.common
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.selection.SelectionContainer
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.venom.utils.getTextDirection
+
+@Composable
+fun DynamicStyledText(
+    text: String,
+    modifier: Modifier = Modifier,
+    textAlign: TextAlign = TextAlign.Unspecified,
+    minFontSize: Int = 24,
+    maxFontSize: Int = 32,
+    lineHeight: Int = 38,
+    fontWeight: FontWeight = FontWeight.Bold,
+    color: Color = MaterialTheme.colorScheme.onBackground,
+) {
+    val dynamicFontSize = (maxFontSize - (text.length / 5)).coerceAtLeast(minFontSize)
+
+    SelectionContainer {
+        Text(
+            text = text,
+            textAlign = textAlign,
+            style = TextStyle(
+                fontSize = dynamicFontSize.sp,
+                color = color,
+                lineHeight = lineHeight.sp,
+                fontWeight = fontWeight,
+                textDirection = getTextDirection(text),
+                shadow = Shadow(
+                    color = MaterialTheme.colorScheme.onBackground.copy(alpha = 0.2f),
+                    offset = Offset(2f, 2f),
+                    blurRadius = 4f
+                )
+            ),
+            modifier = modifier.padding(8.dp)
+        )
+    }
+}
