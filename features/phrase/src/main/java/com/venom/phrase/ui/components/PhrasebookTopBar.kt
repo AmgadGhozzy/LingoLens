72970ff4473db5d0a1c6858907ac1fcf0dc77706
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2025-01-11 23:09:37 +0200

    Refactor: Improve Phrasebook Top Bar
    Refactored PhrasebookTopBar for better readability and UI.

--- features/phrase/src/main/java/com/venom/phrase/ui/components/PhrasebookTopBar.kt
+++ features/phrase/src/main/java/com/venom/phrase/ui/components/PhrasebookTopBar.kt
@@ -1,15 +1,10 @@
 package com.venom.phrase.ui.components
 
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Badge
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarScrollBehavior
@@ -17,10 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.input.nestedscroll.nestedScroll
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import com.venom.resources.R
 import com.venom.ui.components.bars.LanguageBar
 import com.venom.ui.viewmodel.LangSelectorViewModel
 
@@ -30,53 +22,32 @@ fun PhrasebookTopBar(
     viewModel: LangSelectorViewModel,
     totalPhrases: Int,
     scrollBehavior: TopAppBarScrollBehavior,
+    modifier: Modifier = Modifier
 ) {
     TopAppBar(
-        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
         scrollBehavior = scrollBehavior,
-        title = { PhrasesAvailableRow(totalPhrases) },
+        title = {
+            PhrasebookTitle(
+                totalPhrases = totalPhrases
+            )
+        },
         actions = {
             LanguageBar(
                 viewModel = viewModel,
                 modifier = Modifier
-                    .width(224.dp)
+                    .width(256.dp)
                     .clip(RoundedCornerShape(28.dp))
                     .border(
-                        0.5.dp,
-                        MaterialTheme.colorScheme.outlineVariant,
-                        RoundedCornerShape(28.dp)
+                        width = 0.5.dp,
+                        color = MaterialTheme.colorScheme.outlineVariant.copy(alpha = 0.6f),
+                        shape = RoundedCornerShape(28.dp)
                     )
             )
         },
         colors = TopAppBarDefaults.topAppBarColors(
             containerColor = MaterialTheme.colorScheme.surface,
             scrolledContainerColor = MaterialTheme.colorScheme.surface
-        ),
-    )
-}
-
-@Composable
-private fun PhrasesAvailableRow(totalPhrases: Int) {
-    Column {
-        Text(
-            text = stringResource(R.string.phrase_title),
-            style = MaterialTheme.typography.headlineMedium,
-            fontWeight = FontWeight.Bold
         )
-        Row(
-            horizontalArrangement = Arrangement.spacedBy(4.dp)
-        ) {
-
-            Badge(
-                containerColor = MaterialTheme.colorScheme.primaryContainer,
-                contentColor = MaterialTheme.colorScheme.onPrimaryContainer
-            ) { Text("$totalPhrases") }
-
-            Text(
-                text = stringResource(R.string.phrase),
-                style = MaterialTheme.typography.bodyMedium,
-                color = MaterialTheme.colorScheme.onSurfaceVariant
-            )
-        }
-    }
+    )
 }
