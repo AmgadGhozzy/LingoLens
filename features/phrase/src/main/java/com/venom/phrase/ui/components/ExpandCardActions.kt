3b25756841c73ef4753832b95c82e2e3df4b4b9f
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2025-01-11 23:15:49 +0200

    Refactor: Improve Expand Card Actions
    Refactored ExpandCardActions to use custom buttons and speak action. Removed ActionTextButton file.

--- features/phrase/src/main/java/com/venom/phrase/ui/components/ExpandCardActions.kt
+++ features/phrase/src/main/java/com/venom/phrase/ui/components/ExpandCardActions.kt
@@ -2,35 +2,33 @@ package com.venom.phrase.ui.components
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.rounded.VolumeUp
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.outlined.FavoriteBorder
+import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import com.venom.resources.R
+import com.venom.ui.components.buttons.BookmarkFilledButton
+import com.venom.ui.components.buttons.CustomFilledIconButton
 
 @Composable
 fun ExpandCardActions(
-    isFavorite: Boolean, onFavoriteClick: () -> Unit
+    isBookmarked: Boolean, onBookmarkClick: () -> Unit, onSpeakClick: () -> Unit
 ) {
     Row(horizontalArrangement = Arrangement.spacedBy(8.dp)) {
-        ActionButton(
-            onClick = { },
-            icon = Icons.AutoMirrored.Rounded.VolumeUp,
-            contentDescription = "Play pronunciation",
-            containerColor = MaterialTheme.colorScheme.primaryContainer,
-            contentColor = MaterialTheme.colorScheme.onPrimaryContainer
+        CustomFilledIconButton(
+            icon = R.drawable.icon_sound,
+            contentDescription = stringResource(id = R.string.action_speak),
+            onClick = onSpeakClick,
+            size = 42.dp,
+            colors = IconButtonDefaults.filledIconButtonColors(
+                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                contentColor = MaterialTheme.colorScheme.onPrimaryContainer
+            )
         )
 
-        ActionButton(
-            onClick = onFavoriteClick,
-            icon = if (isFavorite) Icons.Filled.Favorite else Icons.Outlined.FavoriteBorder,
-            contentDescription = if (isFavorite) "Remove from favorites" else "Add to favorites",
-            containerColor = if (isFavorite) MaterialTheme.colorScheme.primary
-            else MaterialTheme.colorScheme.surfaceVariant,
-            contentColor = if (isFavorite) MaterialTheme.colorScheme.onPrimary
-            else MaterialTheme.colorScheme.onSurfaceVariant
+        BookmarkFilledButton(
+            isBookmarked = isBookmarked, onToggleBookmark = onBookmarkClick, size = 42.dp
         )
     }
 }
