1c92a5405f952836267b5b1d19a9b12e7c7aa9b6
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2025-01-11 23:06:54 +0200

    Add: TTS, copy, and share actions
    Added TTS, copy, and share functionality to the Phrases screen.

--- features/phrase/src/main/java/com/venom/phrase/ui/screen/PhraseScreen.kt
+++ features/phrase/src/main/java/com/venom/phrase/ui/screen/PhraseScreen.kt
@@ -6,25 +6,39 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.platform.LocalContext
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.venom.phrase.ui.components.PhraseScreenContent
 import com.venom.phrase.ui.viewmodel.PhraseViewModel
+import com.venom.ui.viewmodel.TTSViewModel
+import com.venom.utils.Extensions.copyToClipboard
+import com.venom.utils.Extensions.shareText
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun PhrasesScreen(
-    viewModel: PhraseViewModel = hiltViewModel(), categoryId: Int
+    viewModel: PhraseViewModel = hiltViewModel(),
+    ttsViewModel: TTSViewModel = hiltViewModel(),
+    categoryId: Int
 ) {
     val state by viewModel.state.collectAsState()
+    val context = LocalContext.current
     val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior()
 
     LaunchedEffect(categoryId) {
         viewModel.loadSectionsWithPhrases(categoryId)
     }
 
+    val copyAction: (String) -> Unit = { text -> context.copyToClipboard(text) }
+    val shareAction: (String) -> Unit = { text -> context.shareText(text) }
+
     PhraseScreenContent(
         state = state,
         scrollBehavior = scrollBehavior,
-        onSearchQueryChanged = viewModel::onSearchQueryChange
+        onBookmarkClick = viewModel::toggleRemembered,
+        onSpeakClick = ttsViewModel::speak,
+        onSearchQueryChanged = viewModel::onSearchQueryChange,
+        onCopy = copyAction,
+        onShare = shareAction
     )
 }
