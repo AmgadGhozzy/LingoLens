4226bef9f13163b6a81c2405b84fdb02295c1dae
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2025-01-14 23:58:32 +0200

    feat: Add AdsManager for ad initialization
    Adds AdsManager to handle the initialization of the Mobile Ads SDK.

---
+++ core/data/src/main/java/com/venom/data/AdsManager.kt
@@ -0,0 +1,28 @@
+package com.venom.data
+
+import android.content.Context
+import android.util.Log
+import com.google.android.gms.ads.MobileAds
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class AdsManager @Inject constructor() {
+    private var isInitialized = false
+
+    fun initialize(context: Context, onComplete: () -> Unit = {}) {
+        if (!isInitialized) {
+            val applicationContext = context.applicationContext
+            MobileAds.initialize(applicationContext) {
+                isInitialized = true
+                Log.d(TAG, "Mobile Ads SDK Version: ${MobileAds.getVersion()}")
+                onComplete()
+            }
+        }
+    }
+
+    companion object {
+        private const val TAG = "AdsManager"
+        const val TEST_AD_UNIT_ID = "ca-app-pub-3940256099942544/9214589741"
+    }
+}
