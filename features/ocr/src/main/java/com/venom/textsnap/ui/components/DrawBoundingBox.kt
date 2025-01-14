8cbff8776d3da66a0088e8e0e5d3eb7c0e1b1074
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2025-01-14 23:34:16 +0200

    Refactor: Use sampled background color
    The bounding box now uses a sampled background color, instead of a predefined one. Also adds an imageBitmap to calculate the color.

--- features/ocr/src/main/java/com/venom/textsnap/ui/components/DrawBoundingBox.kt
+++ features/ocr/src/main/java/com/venom/textsnap/ui/components/DrawBoundingBox.kt
@@ -1,8 +1,6 @@
 package com.venom.textsnap.ui.components
 
-import android.util.Log
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,9 +10,12 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.luminance
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.font.FontWeight
@@ -22,57 +23,65 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import com.venom.data.model.ParagraphBox
-
+import com.venom.textsnap.ui.components.sections.sampleTextColors
 
 @Composable
 fun DrawBoundingBox(
     box: ParagraphBox,
     rect: Rect,
+    imageBitmap: ImageBitmap,
     isSelected: Boolean,
-    isParageraphMode: Boolean,
+    isParagraphMode: Boolean,
     showLabels: Boolean,
     onSelect: () -> Unit
 ) {
     val density = LocalDensity.current
-    val textSize = if (isParageraphMode) box.avgWordHeight * 1.4f else rect.height * 0.7f
+    val textSize = if (isParagraphMode) box.avgWordHeight * 95f else rect.height * 0.65f
 
-    Log.d("BoundingBox", "box=$box, rect=$rect, isSelected=$isSelected")
-    Log.d("BoundingBox", "textSize: $textSize")
-
-    Box(modifier = Modifier
-        .offset {
-            IntOffset(
-                rect.left
-                    .toDp()
-                    .roundToPx(),
-                rect.top
-                    .toDp()
-                    .roundToPx()
-            )
-        }
-        .size(width = with(density) { rect.width.toDp() },
-            height = with(density) { rect.height.toDp() })
-        .pointerInput(Unit) { detectTapGestures(onTap = { onSelect() }) }
-        .border(
-            (if (isSelected) 2 else 1).dp,
-            if (isSelected) Color.Green else Color.Red,
-            RoundedCornerShape(6.dp)
+    // Get coordinates from bounding block
+    val vertices = box.boundingBlock.vertices
+    val boxRect = remember(vertices) {
+        Rect(
+            left = vertices[0].x.toFloat(),
+            top = vertices[0].y.toFloat(),
+            right = vertices[2].x.toFloat(),
+            bottom = vertices[2].y.toFloat()
         )
-        .background(
-            if (isSelected) Color.Green.copy(alpha = 0.4f)
-            else Color.Black.copy(alpha = 0.2f), RoundedCornerShape(6.dp)
-        )) {
+    }
 
-        if (showLabels) SelectionContainer {
-            Text(text = box.text,
-                color = Color.White,
-                fontSize = with(density) { textSize.toSp() }, // Dinamic font size based on bounding box height
-                fontWeight = FontWeight.Bold,
-                modifier = Modifier.fillMaxSize(),
-                textAlign = TextAlign.Center,
-                lineHeight = with(density) { textSize.toSp() })
+    val colors = remember(imageBitmap, boxRect) {
+        sampleTextColors(imageBitmap, boxRect)
+    }
+    if (showLabels) {
+        Box(modifier = Modifier
+            .offset {
+                IntOffset(
+                    rect.left
+                        .toDp()
+                        .roundToPx(),
+                    rect.top
+                        .toDp()
+                        .roundToPx()
+                )
+            }
+            .size(width = with(density) { rect.width.toDp() },
+                height = with(density) { rect.height.toDp() })
+            .pointerInput(Unit) { detectTapGestures(onTap = { onSelect() }) }
+
+            .background(
+                if (isSelected) colors.backgroundColor
+                else colors.backgroundColor, RoundedCornerShape(6.dp)
+            )) {
+
+            SelectionContainer {
+                Text(text = box.text,
+                    color = colors.textColor,
+                    fontSize = with(density) { textSize.toSp() },
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.fillMaxSize(),
+                    textAlign = TextAlign.Center,
+                    lineHeight = with(density) { textSize.toSp() })
+            }
         }
     }
 }
-
-
