50de635b50a403d5b640d7139d7d5636b72b4729
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2025-01-17 23:42:27 +0200

    feat: Add Modifier Parameter to ExpandCardActions
    Added a modifier parameter to ExpandCardActions.
    This allows for more flexible styling and layout adjustments of the actions row.

--- features/phrase/src/main/java/com/venom/phrase/ui/components/ExpandCardActions.kt
+++ features/phrase/src/main/java/com/venom/phrase/ui/components/ExpandCardActions.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.venom.resources.R
@@ -13,9 +14,12 @@ import com.venom.ui.components.buttons.CustomFilledIconButton
 
 @Composable
 fun ExpandCardActions(
-    isBookmarked: Boolean, onBookmarkClick: () -> Unit, onSpeakClick: () -> Unit
+    isBookmarked: Boolean,
+    onBookmarkClick: () -> Unit,
+    onSpeakClick: () -> Unit,
+    modifier: Modifier = Modifier
 ) {
-    Row(horizontalArrangement = Arrangement.spacedBy(8.dp)) {
+    Row(horizontalArrangement = Arrangement.spacedBy(8.dp), modifier = modifier) {
         CustomFilledIconButton(
             icon = R.drawable.icon_sound,
             contentDescription = stringResource(id = R.string.action_speak),
