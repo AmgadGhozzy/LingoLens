996fb95647cf1346b8ae3d92a3d1ecac62f8ea43
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2025-01-19 23:35:14 +0200

    feat: Initialize AdsManager
    Initializes the AdsManager in the application's onCreate method.

--- app/src/main/java/com/venom/lingolens/Application.kt
+++ app/src/main/java/com/venom/lingolens/Application.kt
@@ -1,11 +1,16 @@
 package com.venom.lingolens
 
 import android.app.Application
+import com.venom.data.AdsManager
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class Application : Application() {
+
+    var adsManager: AdsManager = AdsManager()
+
     override fun onCreate() {
         super.onCreate()
+        adsManager.initialize(this)
     }
 }
