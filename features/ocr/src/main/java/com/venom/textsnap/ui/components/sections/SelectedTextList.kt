3d39787bacb8b8d37d0be1298725732a472ec3a7
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2025-01-24 09:57:34 +0200

    feat(ui): Add actions to SelectedTextItem
    Pass actions callbacks to SelectedTextItem and update list
    UI layout. Also refactor action implementation.

--- features/ocr/src/main/java/com/venom/textsnap/ui/components/sections/SelectedTextList.kt
+++ features/ocr/src/main/java/com/venom/textsnap/ui/components/sections/SelectedTextList.kt
@@ -3,29 +3,59 @@ package com.venom.textsnap.ui.components.sections
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.venom.resources.R
 import com.venom.textsnap.ui.components.SelectedTextItem
-import com.venom.textsnap.ui.components.TextAction
+import com.venom.ui.components.common.ActionItem
 
 @Composable
 fun SelectedTextList(
-    texts: List<String>, isSingleSelection: Boolean, onAction: (String, TextAction) -> Unit
+    texts: List<String>,
+    isSingleSelection: Boolean,
+    onCopy: (String) -> Unit,
+    onShare: (String) -> Unit,
+    onSpeak: (String) -> Unit,
+    onTranslate: (String) -> Unit
 ) {
-
     LazyColumn(
         modifier = Modifier
             .fillMaxWidth()
+            .padding(horizontal = 8.dp)
             .navigationBarsPadding(),
-        verticalArrangement = Arrangement.spacedBy(6.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
     ) {
         items(texts.withIndex().toList(), key = { it.index }) { (_, text) ->
-            SelectedTextItem(text = text,
+            SelectedTextItem(
+                text = text,
                 expanded = isSingleSelection,
-                onAction = { action -> onAction(text, action) })
+                actions = listOf(
+                    ActionItem.Action(
+                        icon = R.drawable.icon_copy,
+                        description = R.string.action_copy,
+                        onClick = { onCopy(text) }
+                    ),
+                    ActionItem.Action(
+                        icon = R.drawable.icon_sound,
+                        description = R.string.action_speak,
+                        onClick = { onSpeak(text) }
+                    ),
+                    ActionItem.Action(
+                        icon = R.drawable.icon_translate,
+                        description = R.string.action_translate,
+                        onClick = { onTranslate(text) }
+                    ),
+                    ActionItem.Action(
+                        icon = R.drawable.icon_share,
+                        description = R.string.action_share,
+                        onClick = { onShare(text) }
+                    )
+                )
+            )
         }
     }
 }
