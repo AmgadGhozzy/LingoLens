5636e65202cc5bd9d5fd2aaa9646f6308d49ca78
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2025-01-28 20:27:55 +0200

    feat(ui): Add settings button to top bar
    Adds a settings button to the top bar, alongside the existing
    bookmark button. This allows users to access app settings
    directly from the main screen. Also removes navigation from topbar and adds actions callbacks

--- app/src/main/java/com/venom/lingolens/navigation/LingoLensTopBar.kt
+++ app/src/main/java/com/venom/lingolens/navigation/LingoLensTopBar.kt
@@ -2,34 +2,32 @@ package com.venom.lingolens.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
-import androidx.navigation.NavHostController
 import com.venom.resources.R
 import com.venom.ui.components.bars.TopBar
 import com.venom.ui.components.buttons.CustomButton
-import com.venom.ui.navigation.Screen
 
 @Composable
 fun LingoLensTopBar(
-    navController: NavHostController, onBookmarkClick: () -> Unit
+    onBookmarkClick: () -> Unit, onSettingsClick: () -> Unit
 ) {
     TopBar(title = stringResource(id = R.string.app_name),
         leadingIcon = R.drawable.icon_menu,
-        onLeadingIconClick = {
-            navController.navigateToStart(Screen.Translation)
-        },
+        onLeadingIconClick = { onSettingsClick },
         actions = {
-            TopBarActions(onBookmarkClick)
+            TopBarActions(onBookmarkClick, onSettingsClick)
         })
 }
 
 @Composable
-fun TopBarActions(onBookmarkClick: () -> Unit) {
+fun TopBarActions(onBookmarkClick: () -> Unit, onSettingsClick: () -> Unit) {
     CustomButton(
         icon = R.drawable.icon_bookmark_filled,
         contentDescription = stringResource(R.string.bookmarks_title),
         onClick = onBookmarkClick
     )
-    CustomButton(icon = R.drawable.icon_settings,
+    CustomButton(
+        icon = R.drawable.icon_settings,
         contentDescription = stringResource(R.string.settings_title),
-        onClick = {})
+        onClick = onSettingsClick
+    )
 }
