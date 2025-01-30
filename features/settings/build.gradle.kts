5ce3b276ad7fef279a28a55b11b55660f1faa76c
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2025-01-30 23:45:01 +0200

    feat(settings): Add settings feature module
    This commit introduces the settings feature module. It
    includes the necessary build configuration, gradle file and
    gitignore entries for the build directory and .kotlin directory,
    and core dependencies.

---
+++ features/settings/build.gradle.kts
@@ -0,0 +1,93 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+    id("kotlin-kapt")
+    id("com.google.dagger.hilt.android")
+    id("dagger.hilt.android.plugin")
+}
+
+android {
+    namespace = "com.venom.settings"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 24
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            isShrinkResources = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+
+    // Core Modules
+    implementation(project(":core:data"))
+    implementation(project(":core:ui"))
+    implementation(project(":core:di"))
+    implementation(project(":core:resources"))
+
+
+    // Android Jetpack
+    api(libs.androidx.core.ktx)
+    api(libs.androidx.lifecycle.runtime.ktx)
+    api(platform(libs.compose.bom))
+    api(libs.androidx.lifecycle.runtime.compose)
+    api(libs.androidx.lifecycle.viewmodel.compose)
+    api(libs.androidx.lifecycle.viewmodel.ktx)
+    api(libs.androidx.fragment.ktx)
+
+
+    // Hilt
+    api(libs.hilt.android)
+    implementation(libs.kotlinx.serialization.json)
+    kapt(libs.hilt.android.compiler)
+    api(libs.hilt.navigation.compose)
+
+    // Compose
+    api(libs.compose.ui)
+    api(libs.compose.ui.graphics)
+    api(libs.compose.ui.tooling.preview)
+    api(libs.compose.material3)
+    api(libs.compose.animation)
+    api(libs.coil.compose)
+    api(libs.compose.material.icons.extended) {
+        exclude(group = "androidx.compose.material.icons", module = "filled")
+        exclude(group = "androidx.compose.material.icons", module = "outlined")
+        exclude(group = "androidx.compose.material.icons", module = "round")
+        exclude(group = "androidx.compose.material.icons", module = "sharp")
+        exclude(group = "androidx.compose.material.icons", module = "twotone")
+    }
+
+
+    // Kotlin
+    api(libs.kotlin.stdlib)
+
+    // Testing
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.compose.bom))
+    androidTestImplementation(libs.compose.ui.test.junit4)
+    debugImplementation(libs.compose.ui.tooling)
+    debugImplementation(libs.compose.ui.test.manifest)
+}
