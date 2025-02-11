c3cd59a6d919ba5e9a97f6806bd5e7c6b6a65834
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2025-02-12 00:04:55 +0200

    feat: Implement Settings Preferences
    Introduce SettingsPreferences data class to store user preferences,
    including theme, language, and feature settings.  Also introduces
    ThemePreference and PersonalPreference classes.

--- core/data/src/main/java/com/venom/data/model/SettingsPreferences.kt
+++ core/data/src/main/java/com/venom/data/model/SettingsPreferences.kt
@@ -1,15 +1,51 @@
-
 package com.venom.data.model
 
+import com.venom.domain.model.AppLanguage
+import com.venom.domain.model.AppTheme
+import com.venom.domain.model.ColorStyle
+import com.venom.domain.model.FontFamilyStyle
+import com.venom.domain.model.UserLanguage
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.Transient
+import java.util.Locale
 
 @Serializable
 data class SettingsPreferences(
-    val isDarkMode: Boolean = false,
-    val isAutoTheme: Boolean = true,
+
+    val themePrefs: ThemePreference = ThemePreference(),
+
     val speechRate: Float = 1.0f,
-    val selectedLanguage: String = "English",
-    @Transient
+
+    // Language Settings
+    val appLanguage: AppLanguage = AppLanguage.FOLLOW_SYSTEM,
+    val userLanguage: UserLanguage = UserLanguage.entries.firstOrNull {
+        it.isoCode == Locale.getDefault().language
+    } ?: UserLanguage.NOT_SPECIFIED,
+
+    // Personal Settings
+    val personalPrefs: PersonalPreference = PersonalPreference(),
+
+    // Feature Settings
+    val autoPronounciation: Boolean = true,
+    val autoTranslate: Boolean = true,
+    val autoCopy: Boolean = true,
     val lastUpdated: Long = System.currentTimeMillis()
+)
+
+@Serializable
+data class ThemePreference(
+    val appTheme: AppTheme = AppTheme.FOLLOW_SYSTEM,
+    val extractWallpaperColor: Boolean = false,
+    val isAmoledBlack: Boolean = false,
+    val randomColor: Boolean = false,
+    val colorfulBackground: Boolean = false,
+    val colorStyle: ColorStyle = ColorStyle.TonalSpot,
+    val fontFamily: FontFamilyStyle = FontFamilyStyle.SANS_SERIF,
+    val primaryColor: Int = 0xFF00C853.toInt()
+)
+
+@Serializable
+data class PersonalPreference(
+    val maxStreakCount: Int = 0,
+    val currentStreakCount: Int = 0,
+    val lastLoginDate: Long = 0L
 )
