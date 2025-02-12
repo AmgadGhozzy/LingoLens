b056ec08f910c5293d48b41ded418fde99fd4f03
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2025-02-13 01:14:44 +0200

    feat: Create FontFamilyStyle enum class
    Description: Creates an enum class `FontFamilyStyle` that defines a set of font family styles with their titles.

---
+++ core/domain/src/main/java/com/venom/domain/model/FontFamilyStyle.kt
@@ -0,0 +1,10 @@
+package com.venom.domain.model
+
+enum class FontFamilyStyle(val title: String) {
+    SANS_SERIF("Sans Serif"),
+    ROBOTO("Roboto"),
+    MONOSPACE("Monospace"),
+    QUICKSAND("Quicksand"),
+    CAVEAT("Caveat"),
+    JOSEFIN_SANS("Josefin Sans")
+}
