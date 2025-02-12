7b615cd9d4481425cee43a3af5cc164a9e03bb66
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2025-02-13 00:28:56 +0200

    feat(domain): Add AppTheme enum
    This enum defines the available application themes: Dark, Light, and Follow System.  It will be used to manage the user's theme preference.

---
+++ core/domain/src/main/java/com/venom/domain/model/AppTheme.kt
@@ -0,0 +1,10 @@
+package com.venom.domain.model
+
+import androidx.annotation.StringRes
+import com.venom.resources.R
+
+enum class AppTheme(@StringRes val title: Int) {
+    DARK(R.string.dark_mode),
+    LIGHT(R.string.light_mode),
+    FOLLOW_SYSTEM(R.string.follow_system)
+}
