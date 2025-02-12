e0f3974360d67dcc94542e285f9b9073732e0a18
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2025-02-13 01:14:57 +0200

    feat: Create ColorStyle enum class for UI theme options
    Description: Introduce the `ColorStyle` enum to represent different UI theme options, including "Tonal spot," "Neutral," "Vibrant," "Expressive," "Rainbow," "Fruit salad," "Monochrome," "Fidelity," and "Content." Each option has a corresponding title for display purposes.

---
+++ core/domain/src/main/java/com/venom/domain/model/ColorStyle.kt
@@ -0,0 +1,13 @@
+package com.venom.domain.model
+
+enum class ColorStyle(val title: String) {
+    TonalSpot("Tonal spot"),
+    Neutral("Neutral"),
+    Vibrant("Vibrant"),
+    Expressive("Expressive"),
+    Rainbow("Rainbow"),
+    FruitSalad("Fruit salad"),
+    Monochrome(""),
+    Fidelity("Fidelity"),
+    Content("Content")
+}
