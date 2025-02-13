d0f5cd40269f138c8255e590803342236f72d14d
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2025-02-13 09:39:59 +0200

    feat(settings): Refactor SettingsScaffold for improved UI and flexibility
    - Replaced `CenterAlignedTopAppBar` with a custom `Box` for the title bar, allowing for more control over layout and styling.
    - Modified `SettingsScaffold` to use a `LazyColumn` for content, enabling better performance and scrollability, especially with long lists.
    - Added `StringRes` for title to enable localization.
    - Replaced the previous basic content parameter for `SettingsScaffold` with a `LazyListScope`, allowing for the use of `items()` and other `LazyColumn` DSL functions.
    - Added content paddings.
    - Applied Material Design 3 theming to the `SettingsScaffold`.

--- features/settings/src/main/java/com/venom/settings/presentation/components/SettingsScaffold.kt
+++ features/settings/src/main/java/com/venom/settings/presentation/components/SettingsScaffold.kt
@@ -1,40 +1,78 @@
 package com.venom.settings.presentation.components
 
+import androidx.annotation.StringRes
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Close
-import androidx.compose.material3.CenterAlignedTopAppBar
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import com.venom.resources.R
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SettingsScaffold(
-    title: String = stringResource(id = R.string.settings),
+    @StringRes title: Int = R.string.settings,
     onDismiss: () -> Unit,
-    content: @Composable () -> Unit
+    content: LazyListScope.() -> Unit
 ) {
     Column(
         modifier = Modifier
             .fillMaxWidth()
-            .systemBarsPadding()
+            .animateContentSize()
     ) {
-        CenterAlignedTopAppBar(
-            title = { Text(title) },
-            actions = {
-                IconButton(onClick = onDismiss) {
-                    Icon(Icons.Rounded.Close, stringResource(id = R.string.action_close))
-                }
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(48.dp)
+                .background(MaterialTheme.colorScheme.surfaceContainerLow)
+                .padding(horizontal = 16.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                text = stringResource(title),
+                style = MaterialTheme.typography.titleMedium,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center
+            )
+
+            IconButton(
+                onClick = onDismiss,
+                modifier = Modifier.align(Alignment.CenterEnd)
+            ) {
+                Icon(
+                    Icons.Rounded.Close,
+                    contentDescription = stringResource(id = R.string.action_close)
+                )
             }
+        }
+
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(MaterialTheme.colorScheme.surfaceContainerLow)
+                .animateContentSize(),
+            contentPadding = PaddingValues(16.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            content = content
         )
-        content()
     }
 }
