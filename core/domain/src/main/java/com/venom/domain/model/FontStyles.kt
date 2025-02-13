7d9b1f3cc268a05be89b284f74deecc4a4db7192
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2025-02-13 23:57:07 +0200

    refactor(domain): Rename FontFamilyStyle to FontStyles and update values
    Renamed the `FontFamilyStyle` enum to `FontStyles` for better clarity and added new font style options: Default, Serif, and Cursive.  This change provides a more comprehensive set of font styles for users to choose from.

---
+++ core/domain/src/main/java/com/venom/domain/model/FontStyles.kt
@@ -0,0 +1,14 @@
+package com.venom.domain.model
+
+enum class FontStyles(val title: String) {
+    Default("Default"),
+    SANS_SERIF("Sans Serif"),
+    SERIF("Serif"),
+    MONOSPACE("Monospace"),
+    CURSIVE("Cursive"),
+
+    CAVEAT("Caveat"),
+    ROBOTO("Roboto"),
+    QUICKSAND("Quicksand"),
+    JOSEFIN_SANS("Josefin Sans")
+}
