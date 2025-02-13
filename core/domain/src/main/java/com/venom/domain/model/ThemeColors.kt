39adccbad97915b002f804e1ac98cd9392a3f8af
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2025-02-14 00:19:32 +0200

    feat: Add theme color model and generator
    Description: Creates the `ThemeColor` data model and a function to generate a list of theme colors.

---
+++ core/domain/src/main/java/com/venom/domain/model/ThemeColors.kt
@@ -0,0 +1,24 @@
+package com.venom.domain.model
+
+data class ThemeColor(
+    val name: String,
+    val color: Long
+)
+
+fun generateThemeColors() = listOf(
+    ThemeColor("Soft Pink", 0xFFFFC6E5),
+    ThemeColor("Dusty Rose", 0xFFE67AB2),
+    ThemeColor("Orchid", 0xFFD186E0),
+    ThemeColor("Bright Lavender", 0xFFD975FF),
+    ThemeColor("Periwinkle", 0xFFB088FF),
+    ThemeColor("Cornflower", 0xFF6B8CFF),
+    ThemeColor("Royal Blue", 0xFF5E82F0),
+    ThemeColor("Sky Blue", 0xFF00B8FF),
+    ThemeColor("Ocean Blue", 0xFF0099EE),
+    ThemeColor("Turquoise", 0xFF1EEBB8),
+    ThemeColor("Teal", 0xFF00C4AA),
+    ThemeColor("Emerald", 0xFF00E880),
+    ThemeColor("Jade", 0xFF00CC66),
+    ThemeColor("Lime", 0xFFC8FF00),
+    ThemeColor("Chartreuse", 0xFFB6EE00)
+)
