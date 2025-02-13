fe5b892314719751fc01834cd61b96576ecf57d6
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2025-02-14 00:19:42 +0200

    feat: Add SettingsDialog enum for settings navigation

---
+++ core/domain/src/main/java/com/venom/domain/model/SettingsDialog.kt
@@ -0,0 +1,12 @@
+package com.venom.domain.model
+
+enum class SettingsDialog {
+    None,
+    SelectAppLanguage,
+    SelectNativeLanguage,
+    SelectTheme,
+    SelectPaletteStyle,
+    SelectPrimaryColor,
+    SelectFont,
+    About
+}
