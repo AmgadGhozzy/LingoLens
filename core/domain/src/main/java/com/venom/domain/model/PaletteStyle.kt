5ddadf621da04614c629d4d19bf7604f9d37e22a
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2025-02-14 00:18:54 +0200

    refactor: Rename ColorStyle to PaletteStyle
    Renamed the `ColorStyle` enum to `PaletteStyle` to better
    reflect its purpose and the values it represents. Removed
    unused values.

---
+++ core/domain/src/main/java/com/venom/domain/model/PaletteStyle.kt
@@ -0,0 +1,10 @@
+package com.venom.domain.model
+
+enum class PaletteStyle(val title: String) {
+    Neutral("Neutral"),
+    FruitSalad("Fruit salad"),
+    Vibrant("Vibrant"),
+    Expressive("Expressive"),
+    Rainbow("Rainbow"),
+    Monochrome("Monochrome"),
+}
