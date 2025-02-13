93aed4ffd6076af9acbde2604c12ccea92721450
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2025-02-14 01:38:45 +0200

    feat(core): Add Constants for settings and external links
    Description: This commit introduces a `Constants.kt` file within the core/utils module. This file contains constants for setting file names, and various external links including Github, LinkedIn, License, Privacy, Terms, and Play Store.

---
+++ core/utils/src/main/java/com/venom/utils/Constants.kt
@@ -0,0 +1,16 @@
+package com.venom.utils
+
+const val SETTING_FILE = "settings.pb"
+
+const val EMAIL = "AmgadGhozzy@gmail.com"
+const val GITHUB = "https://github.com/AmgadGhozzy/LingoLens"
+const val LINKEDIN = "https://linkedin.com/in/AmgadGhozzy"
+const val LICENSE = "https://github.com/AmgadGhozzy/LingoLens/blob/main/LICENSE"
+const val PRIVACY = "https://github.com/AmgadGhozzy/LingoLens/blob/main/Privacy.md"
+const val TERMS = "https://github.com/AmgadGhozzy/LingoLens/blob/main/Terms.md"
+
+const val PLAY_STORE = "https://play.google.com/store/apps/details?id=com.venom.lingolens"
+const val PLAY_STORE_Search = "https://play.google.com/store/search?q=venom&c=apps"
+const val PLAY_STORE_SHORT = "https://cutt.ly/"
+const val PLAY_STORE_SEARCH_SHORT = "https://cutt.ly/"
+
