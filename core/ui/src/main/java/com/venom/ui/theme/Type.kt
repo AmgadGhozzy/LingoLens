ced92566453a2fea14265917108f698b21e9cb55
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2025-02-14 02:08:06 +0200

    feat(ui): Add custom font support and typography
    Adds support for custom fonts (Caveat, Roboto, Quicksand,
    JosefinSans) and provides a function to apply these fonts to the
    default Material3 typography. Also adds extension function to
    convert fontStyle to fontFamily.

--- core/ui/src/main/java/com/venom/ui/theme/Type.kt
+++ core/ui/src/main/java/com/venom/ui/theme/Type.kt
@@ -1,34 +1,68 @@
 package com.venom.ui.theme
 
 import androidx.compose.material3.Typography
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
+import com.venom.domain.model.FontStyles
+import com.venom.resources.R
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+private val defaultType = Typography()
+
+val Caveat = FontFamily(
+    Font(R.font.caveat_medium, FontWeight.Medium),
+    Font(R.font.caveat_regular, FontWeight.Normal),
+    Font(R.font.caveat_bold, FontWeight.Bold)
 )
+
+val Roboto = FontFamily(
+    Font(R.font.roboto_medium, FontWeight.Medium),
+    Font(R.font.roboto_light, FontWeight.Light),
+    Font(R.font.roboto_regular, FontWeight.Normal),
+    Font(R.font.roboto_bold, FontWeight.Bold)
+)
+
+val Quicksand = FontFamily(
+    Font(R.font.quicksand_medium, FontWeight.Medium),
+    Font(R.font.quicksand_light, FontWeight.Light),
+    Font(R.font.quicksand_regular, FontWeight.Normal),
+    Font(R.font.quicksand_bold, FontWeight.Bold)
+)
+
+val JosefinSans = FontFamily(
+    Font(R.font.josefin_sans_medium, FontWeight.Medium),
+    Font(R.font.josefin_sans_light, FontWeight.Light),
+    Font(R.font.josefin_sans_regular, FontWeight.Normal),
+    Font(R.font.josefin_sans_bold, FontWeight.Bold)
+)
+
+fun getTypography(fontFamily: FontFamily) = defaultType.copy(
+    displayLarge = defaultType.displayLarge.copy(fontFamily = fontFamily),
+    displayMedium = defaultType.displayMedium.copy(fontFamily = fontFamily),
+    displaySmall = defaultType.displaySmall.copy(fontFamily = fontFamily),
+    headlineLarge = defaultType.headlineLarge.copy(fontFamily = fontFamily),
+    headlineMedium = defaultType.headlineMedium.copy(fontFamily = fontFamily),
+    headlineSmall = defaultType.headlineSmall.copy(fontFamily = fontFamily),
+    titleLarge = defaultType.titleLarge.copy(fontFamily = fontFamily),
+    titleMedium = defaultType.titleMedium.copy(fontFamily = fontFamily),
+    titleSmall = defaultType.titleSmall.copy(fontFamily = fontFamily),
+    bodyLarge = defaultType.bodyLarge.copy(fontFamily = fontFamily),
+    bodyMedium = defaultType.bodyMedium.copy(fontFamily = fontFamily),
+    bodySmall = defaultType.bodySmall.copy(fontFamily = fontFamily),
+    labelLarge = defaultType.labelLarge.copy(fontFamily = fontFamily),
+    labelMedium = defaultType.labelMedium.copy(fontFamily = fontFamily),
+    labelSmall = defaultType.labelSmall.copy(fontFamily = fontFamily)
+)
+
+fun FontStyles.toFontFamily(): FontFamily = when (this) {
+    FontStyles.Default -> FontFamily.Default
+    FontStyles.SANS_SERIF -> FontFamily.SansSerif
+    FontStyles.SERIF -> FontFamily.Serif
+    FontStyles.MONOSPACE -> FontFamily.Monospace
+    FontStyles.CURSIVE -> FontFamily.Cursive
+
+    FontStyles.CAVEAT -> Caveat
+    FontStyles.ROBOTO -> Roboto
+    FontStyles.QUICKSAND -> Quicksand
+    FontStyles.JOSEFIN_SANS -> JosefinSans
+}
