d61fd71eacf0f617c86522ad243ea049ab1ce868
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2025-02-15 05:12:18 +0200

    chore(db): Disable Room database schema export
    Disables schema export for the Room database. This prevents the
    generation of schema files, which are not currently used and can
    introduce unnecessary complexity.

--- features/stackcard/src/main/java/com/venom/stackcard/data/local/WordDatabase.kt
+++ features/stackcard/src/main/java/com/venom/stackcard/data/local/WordDatabase.kt
@@ -7,7 +7,7 @@ import com.venom.data.local.typeconverter.ListStringConverter
 import com.venom.stackcard.data.local.dao.WordDao
 import com.venom.stackcard.data.model.WordEntity
 
-@Database(entities = [WordEntity::class], version = 1)
+@Database(entities = [WordEntity::class], version = 1, exportSchema = false)
 @TypeConverters(ListStringConverter::class)
 abstract class WordDatabase : RoomDatabase() {
     abstract fun wordDao(): WordDao
