2fcaff6a04d71786ed207213c890b94e768cad52
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2025-02-15 23:50:04 +0200

    feat: Implement Firebase Messaging Service
    Adds MyFirebaseMessagingService to handle push notifications.
    
    The service handles receiving and displaying notifications,
    including setting custom content, sound, and priority.
    It also includes handling the new token for FCM.

---
+++ app/src/main/java/com/venom/lingolens/MyFirebaseMessagingService.kt
@@ -0,0 +1,58 @@
+package com.venom.lingolens
+
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Intent
+import android.media.RingtoneManager
+import androidx.core.app.NotificationCompat
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        remoteMessage.notification?.let { sendNotification(it) }
+    }
+
+    override fun onNewToken(token: String) {
+        super.onNewToken(token)
+    }
+
+    private fun sendNotification(notification: RemoteMessage.Notification) {
+        val intent = Intent(this, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+        }
+
+        val pendingIntent = PendingIntent.getActivity(
+            this, 0, intent, PendingIntent.FLAG_IMMUTABLE
+        )
+
+        val channelId = getString(com.venom.resources.R.string.default_notification_channel_id)
+
+//        val customContentView = android.widget.RemoteViews(packageName, R.layout.notification_small)
+//        val customBigContentView = android.widget.RemoteViews(packageName, R.layout.notification_expanded)
+
+
+        val notificationBuilder = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.mipmap.ic_launcher)
+            // Basic
+            .setContentTitle(notification.title)
+            .setContentText(notification.body)
+            .setContentIntent(pendingIntent)
+            // Additional
+            .setColor(notification.color?.toInt() ?: 0)
+            .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
+            .setChannelId(notification.channelId ?: channelId)
+            .setAutoCancel(true)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+
+        // Custom views
+//            .setCustomContentView(customContentView)
+//            .setCustomBigContentView(customBigContentView)
+
+        getSystemService(NotificationManager::class.java)?.notify(
+            notification.tag?.hashCode() ?: 0,
+            notificationBuilder.build()
+        )
+    }
+}
