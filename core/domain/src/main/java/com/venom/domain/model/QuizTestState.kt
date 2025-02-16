4bbf8065e4643590299399ccf48c7f58658abf56
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2025-02-16 02:38:51 +0200

    refactor(domain): Rename and move QuizTestState
    Moves `TestState` to `QuizTestState` in the core domain module
    and updates its properties for better quiz state management.
    Also renames `NotStarted` to `Initial`.

--- core/domain/src/main/java/com/venom/domain/model/QuizTestState.kt
+++ core/domain/src/main/java/com/venom/domain/model/QuizTestState.kt
@@ -1,19 +1,22 @@
-package com.venom.stackcard.domain.model
+package com.venom.domain.model
 
 sealed class QuizTestState {
-    object NotStarted : QuizTestState()
+    object Initial : QuizTestState()
+
     data class InProgress(
         val currentQuestion: Int,
         val totalQuestions: Int,
         val timeRemaining: Int,
         val hearts: Int,
-        val score: Int,
+        val score: Float,
         val streak: Int
     ) : QuizTestState()
 
     data class Completed(
-        val score: Float,
+        val level: WordLevels,
+        val levelPercentage: Float,
+        val totalQuestions: Int,
         val passed: Boolean,
-        val nextLevel: WordLevels?
+        val nextLevel: WordLevels? = null
     ) : QuizTestState()
 }
