f8e153b61308afc82a2f121a76ce325ad8ef8c57
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2025-02-16 02:40:13 +0200

    feat(stackcard): Add initial level support to CardScreen
    Allows setting an initial difficulty level for words displayed
    in the CardScreen. This is achieved by passing an `initialLevel`
    parameter to the `CardScreen` composable, which is then used to
    set the level in the `wordViewModel` when the screen is launched.

--- features/stackcard/src/main/java/com/venom/stackcard/ui/screen/CardScreen.kt
+++ features/stackcard/src/main/java/com/venom/stackcard/ui/screen/CardScreen.kt
@@ -1,12 +1,14 @@
 package com.venom.stackcard.ui.screen
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.venom.domain.model.WordLevels
 import com.venom.resources.R
 import com.venom.stackcard.ui.viewmodel.CardItem
 import com.venom.stackcard.ui.viewmodel.CardSwiperEvent
@@ -18,12 +20,13 @@ import com.venom.ui.components.sections.TabItem
 import com.venom.ui.viewmodel.TTSViewModel
 import com.venom.utils.Extensions.copyToClipboard
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CardScreen(
     wordViewModel: CardSwiperViewModel = hiltViewModel(),
     phraseViewModel: CardSwiperViewModel = hiltViewModel(),
     ttsViewModel: TTSViewModel = hiltViewModel(),
-    cardItemList: List<CardItem> = emptyList()
+    initialLevel: WordLevels? = null
 ) {
     val context = LocalContext.current
 
@@ -46,6 +49,7 @@ fun CardScreen(
 
     // Set initial CardType for each ViewModel
     LaunchedEffect(selectedTab) {
+        initialLevel?.let { wordViewModel.setLevel(initialLevel)}
         if (selectedTab == 0) wordViewModel.onEvent(SetCardType(CardType.WORD))
         else wordViewModel.onEvent(SetCardType(CardType.PHRASE))
     }
