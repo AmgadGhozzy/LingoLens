a707ebe99a8896cc02a8c1aae8bc99da4c69b4bc
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2025-02-16 22:15:52 +0200

    feat(ui): Update TopBar to display dynamic titles
    The TopBar title and leading icon are now dynamically updated
    based on the current screen. The OCR screen now displays a back
    arrow icon and navigates back when clicked. The title also
    changes to "Quiz" when the user is on the quiz screen and "OCR"
    when on the OCR screen.

--- app/src/main/java/com/venom/lingolens/navigation/LingoLensTopBar.kt
+++ app/src/main/java/com/venom/lingolens/navigation/LingoLensTopBar.kt
@@ -1,21 +1,41 @@
 package com.venom.lingolens.navigation
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.ArrowBackIosNew
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
 import com.venom.resources.R
 import com.venom.ui.components.bars.TopBar
 import com.venom.ui.components.buttons.CustomButton
+import com.venom.ui.navigation.Screen
 
 @Composable
 fun LingoLensTopBar(
-    onBookmarkClick: () -> Unit, onSettingsClick: () -> Unit
+    currentScreen: Screen,
+    onNavigateBack: () -> Unit,
+    onBookmarkClick: () -> Unit,
+    onSettingsClick: () -> Unit
 ) {
-    TopBar(title = stringResource(id = R.string.app_name),
-        leadingIcon = R.drawable.icon_menu,
-        onLeadingIconClick = { onSettingsClick },
+    TopBar(
+        title = stringResource(
+            when (currentScreen) {
+                is Screen.Quiz -> R.string.quiz_title
+                Screen.Ocr -> R.string.ocr_title
+                else -> R.string.app_name
+            }
+        ),
+        leadingIcon = when {
+            currentScreen == Screen.Ocr -> Icons.Rounded.ArrowBackIosNew
+            else -> R.drawable.icon_menu
+        },
+        onLeadingIconClick = when {
+            currentScreen == Screen.Ocr -> onNavigateBack
+            else -> onSettingsClick
+        },
         actions = {
             TopBarActions(onBookmarkClick, onSettingsClick)
-        })
+        }
+    )
 }
 
 @Composable
