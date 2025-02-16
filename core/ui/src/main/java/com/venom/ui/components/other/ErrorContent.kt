d865ab980b4106de6d63febe70acf5c9d23094de
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2025-02-16 02:49:18 +0200

    feat(ui): Add ErrorContent composable
    This commit introduces the `ErrorContent` composable, which
    displays an error message and a retry button. This component
    will be used to handle and display errors to the user, providing
    a clear indication of what went wrong and a way to attempt
    recovery.

---
+++ core/ui/src/main/java/com/venom/ui/components/other/ErrorContent.kt
@@ -0,0 +1,43 @@
+package com.venom.ui.components.other
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.venom.resources.R
+
+@Composable
+fun ErrorContent(
+    error: String,
+    onRetry: () -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = error,
+            color = MaterialTheme.colorScheme.error,
+            style = MaterialTheme.typography.bodyLarge,
+            textAlign = TextAlign.Center
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = onRetry) {
+            Text(stringResource(id = R.string.retry))
+        }
+    }
+}
