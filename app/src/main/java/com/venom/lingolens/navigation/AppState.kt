565975e513fe467d361aa1a44e63ac678a9f95e2
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2025-02-18 06:25:19 +0200

    feat: Add AppState for managing navigation
    This commit introduces the `AppState` class, which is responsible
    for managing the application's navigation state. It holds the
    `NavHostController`, current back stack entry, and flags for
    showing the bookmark history and settings screens. It also provides
    helper methods for navigating to different screens and handling
    back button presses.

---
+++ app/src/main/java/com/venom/lingolens/navigation/AppState.kt
@@ -0,0 +1,53 @@
+package com.venom.lingolens.navigation
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.navigation.NavBackStackEntry
+import androidx.navigation.NavHostController
+import com.venom.ui.navigation.Screen
+
+class AppState(
+    val navController: NavHostController
+) {
+    var currentBackStackEntry: NavBackStackEntry? by mutableStateOf(null)
+    var showBookmarkHistory by mutableStateOf(false)
+    var showSettings by mutableStateOf(false)
+
+    val currentRoute: String?
+        get() = currentBackStackEntry?.destination?.route
+
+    val currentScreen: Screen
+        get() = Screen.fromRoute(currentRoute ?: Screen.Translation.route)
+
+    val shouldShowTopBar: Boolean
+        get() = currentRoute != Screen.Quiz.LevelTest.route
+
+    val shouldShowBottomBar: Boolean
+        get() = currentRoute != Screen.Ocr.route &&
+                currentRoute != Screen.Quiz.LevelTest.route
+
+    fun navigateToScreen(screen: Screen) {
+        when (screen) {
+            is Screen.Translation -> navController.navigate(screen.route) {
+                popUpTo(Screen.Translation.route) { inclusive = true }
+            }
+
+            else -> navController.navigate(screen.route) {
+                popUpTo(Screen.Translation.route)
+            }
+        }
+    }
+
+    fun handleBackPress() {
+        when {
+            showSettings -> showSettings = false
+            showBookmarkHistory -> {
+                showBookmarkHistory = false
+                navigateToScreen(currentScreen)
+            }
+
+            else -> navController.popBackStack()
+        }
+    }
+}
