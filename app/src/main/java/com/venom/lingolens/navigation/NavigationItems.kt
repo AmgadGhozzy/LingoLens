b7a4442c72ccb1cc52daa02488dfc5e69ad4b2e9
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2025-03-20 21:42:36 +0200

    feat(ui): Update quiz icon
    Replaced the dialog icon with a dedicated quiz logo
    for the quiz navigation item.

--- app/src/main/java/com/venom/lingolens/navigation/NavigationItems.kt
+++ app/src/main/java/com/venom/lingolens/navigation/NavigationItems.kt
@@ -3,7 +3,6 @@ package com.venom.lingolens.navigation
 import com.venom.resources.R
 import com.venom.ui.navigation.Screen
 
-
 enum class NavigationItems(
     val screen: Screen,
     val icon: Int,
@@ -11,7 +10,7 @@ enum class NavigationItems(
     val showInBottomBar: Boolean = true
 ) {
     WORDCARD(Screen.StackCard, R.drawable.icon_cards1, R.string.stackcard_title),
-    QUIZ(Screen.Quiz.MainLevel, R.drawable.icon_dialog, R.string.quiz_title),
+    QUIZ(Screen.Quiz.MainLevel, R.drawable.quiz_logo, R.string.quiz_title),
     DIALOG(Screen.Dialog, R.drawable.icon_dialogs_activated, R.string.dialog_title, false),
     PHRASE(Screen.Phrases, R.drawable.icon_dialog, R.string.phrase_title),
     OCR(Screen.Ocr, R.drawable.icon_camera, R.string.ocr_title),
