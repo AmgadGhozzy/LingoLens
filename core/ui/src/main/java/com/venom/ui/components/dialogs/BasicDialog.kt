a0c0a6307d1d6402e693fe5201eb4ba4f47e0590
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2025-03-20 21:44:56 +0200

    refactor(ui): Move BasicDialog to core UI module
    This commit moves the BasicDialog component from the settings
    feature module to the core UI module. This promotes reusability
    across different features within the application.

--- core/ui/src/main/java/com/venom/ui/components/dialogs/BasicDialog.kt
+++ core/ui/src/main/java/com/venom/ui/components/dialogs/BasicDialog.kt
@@ -1,4 +1,4 @@
-package com.venom.settings.presentation.components.dialogs
+package com.venom.ui.components.dialogs
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
