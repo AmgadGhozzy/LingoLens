812db12e552e16f98b2b09a8f55e2f183dbaf799
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2025-03-20 21:43:54 +0200

    feat(data): Add native and target language settings
    Adds methods to set and retrieve native and target
    languages in settings. Updates language model for
    consistency.

--- core/data/src/main/java/com/venom/data/repo/SettingsRepository.kt
+++ core/data/src/main/java/com/venom/data/repo/SettingsRepository.kt
@@ -1,12 +1,12 @@
 package com.venom.data.repo
 
+import com.venom.data.model.LanguageItem
 import com.venom.data.model.PersonalPreference
 import com.venom.data.model.SettingsPreferences
+import com.venom.data.model.ThemeColor
 import com.venom.data.model.ThemePreference
-import com.venom.domain.model.AppLanguage
 import com.venom.domain.model.FontStyles
 import com.venom.domain.model.PaletteStyle
-import com.venom.domain.model.ThemeColor
 import kotlinx.coroutines.flow.Flow
 
 interface SettingsRepository {
@@ -16,9 +16,11 @@ interface SettingsRepository {
     suspend fun updateThemePreferences(update: ThemePreference.() -> ThemePreference)
     suspend fun updatePersonalPreferences(update: PersonalPreference.() -> PersonalPreference)
 
-    suspend fun setAppLanguage(language: AppLanguage)
+    suspend fun setAppLanguage(language: LanguageItem)
+    suspend fun setNativeLanguage(language: LanguageItem)
+    suspend fun setTargetLanguage(language: LanguageItem)
+
     suspend fun setSpeechRate(rate: Float)
-    suspend fun setNativeLanguage(language: AppLanguage)
     suspend fun setPrimaryColor(color: ThemeColor)
     suspend fun setPaletteStyle(style: PaletteStyle)
     suspend fun toggleAmoledBlack()
