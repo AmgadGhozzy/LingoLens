81ac7d9c76b72fbe240421807258af6d07159f14
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2025-04-18 22:15:43 +0200

    feat(ui): Add custom bottom sheet component
    This commit introduces a reusable `CustomBottomSheet` composable.
    It provides a styled bottom sheet with rounded corners and a
    default height, suitable for displaying content within the app.

---
+++ core/ui/src/main/java/com/venom/ui/components/common/CustomBottomSheet.kt
@@ -0,0 +1,39 @@
+package com.venom.ui.components.common
+
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.unit.dp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CustomBottomSheet(
+    onDismiss: () -> Unit,
+    content: @Composable ColumnScope.() -> Unit,
+) {
+    val screenHeight = LocalConfiguration.current.screenHeightDp.dp
+    val sheetHeight = screenHeight * 0.98f
+
+    val sheetState = rememberModalBottomSheetState(
+        skipPartiallyExpanded = true
+    )
+
+    ModalBottomSheet(
+        modifier = Modifier
+            .height(sheetHeight)
+            .offset(y = screenHeight * 0.06f)
+            .navigationBarsPadding(),
+        sheetState = sheetState,
+        onDismissRequest = onDismiss,
+        tonalElevation = 2.dp,
+        containerColor = MaterialTheme.colorScheme.surfaceContainerLow,
+        shape = RoundedCornerShape(topStart = 28.dp, topEnd = 28.dp),
+        content = content
+    )
+}
