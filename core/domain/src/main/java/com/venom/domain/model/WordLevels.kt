2d9fe443d04f392a341a72ea22896ca65958bda1
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2025-04-26 22:37:26 +0300

    feat: Add icons to WordLevels
    This commit introduces icons for each WordLevel.
    The icon is now a resource ID instead of an ImageVector.
    This improves the visual representation of word levels
    in the application.

--- core/domain/src/main/java/com/venom/domain/model/WordLevels.kt
+++ core/domain/src/main/java/com/venom/domain/model/WordLevels.kt
@@ -3,48 +3,58 @@ package com.venom.domain.model
 import com.venom.resources.R
 
 sealed class WordLevels(
-    val range: LevelRange, val titleRes: Int, val descRes: Int, val id: String
+    val range: LevelRange,
+    val titleRes: Int,
+    val descRes: Int,
+    val id: String,
+    val iconRes: Int  // Changed to use resource ID instead of ImageVector
 ) {
     object Beginner : WordLevels(
         range = LevelRange(1, 1000),
         titleRes = R.string.level_beginner,
         descRes = R.string.desc_beginner,
-        id = "beginner"
+        id = "beginner",
+        iconRes = R.drawable.ic_beginner_emoji_people
     )
 
     object Elementary : WordLevels(
         range = LevelRange(1001, 2000),
         titleRes = R.string.level_elementary,
         descRes = R.string.desc_elementary,
-        id = "elementary"
+        id = "elementary",
+        iconRes = R.drawable.ic_elementary_school
     )
 
     object Intermediate : WordLevels(
         range = LevelRange(2001, 3000),
         titleRes = R.string.level_intermediate,
         descRes = R.string.desc_intermediate,
-        id = "intermediate"
+        id = "intermediate",
+        iconRes = R.drawable.ic_intermediate_auto_stories
     )
 
     object UpperIntermediate : WordLevels(
         range = LevelRange(3001, 4000),
         titleRes = R.string.level_upper_intermediate,
         descRes = R.string.desc_upper_intermediate,
-        id = "upper_intermediate"
+        id = "upper_intermediate",
+        iconRes = R.drawable.ic_upper_intermediate_psychology
     )
 
     object Advanced : WordLevels(
         range = LevelRange(4001, 5000),
         titleRes = R.string.level_advanced,
         descRes = R.string.desc_advanced,
-        id = "advanced"
+        id = "advanced",
+        iconRes = R.drawable.ic_advanced_architecture
     )
 
     object Mastery : WordLevels(
         range = LevelRange(5001, 6000),
         titleRes = R.string.level_mastery,
         descRes = R.string.desc_mastery,
-        id = "mastery"
+        id = "mastery",
+        iconRes = R.drawable.ic_mastery_trophy
     )
 
     data class LevelRange(val start: Int, val end: Int)
