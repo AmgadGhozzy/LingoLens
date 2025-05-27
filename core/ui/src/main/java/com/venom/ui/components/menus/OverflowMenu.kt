0b95ddfc5dbcc1807cca9b5870b23a26d9b1cdae
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2025-05-27 19:36:19 +0300

    Refactor: Replace CustomDropdownMenu with AnimatedDropdownMenu in OverflowMenu.

--- core/ui/src/main/java/com/venom/ui/components/menus/OverflowMenu.kt
+++ core/ui/src/main/java/com/venom/ui/components/menus/OverflowMenu.kt
@@ -1,25 +1,13 @@
 package com.venom.ui.components.menus
 
-import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.animation.core.tween
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.MoreHoriz
 import androidx.compose.material.icons.rounded.MoreVert
-import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.TransformOrigin
-import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
 import com.venom.resources.R
 import com.venom.ui.components.buttons.CustomButton
@@ -33,12 +21,6 @@ fun OverflowMenu(
     buttonType: ButtonType = ButtonType.FILLED_ICON
 ) {
     var isExpanded by remember { mutableStateOf(false) }
-
-    val rollAnimation by animateFloatAsState(
-        targetValue = if (isExpanded) 1f else 0f,
-        animationSpec = tween(durationMillis = 600),
-        label = "Roll Animation"
-    )
 
     Box(modifier = modifier) {
         when (buttonType) {
@@ -57,58 +39,11 @@ fun OverflowMenu(
             )
         }
 
-        DropdownMenu(
+        AnimatedDropdownMenu(
             expanded = isExpanded,
             onDismissRequest = { isExpanded = false },
-            offset = DpOffset(x = 0.dp, y = 8.dp),
-            shape = RoundedCornerShape(12.dp),
-            modifier = Modifier
-                .background(color = MaterialTheme.colorScheme.surface)
-                .alpha(rollAnimation)
-                .graphicsLayer(
-                    scaleY = rollAnimation, transformOrigin = TransformOrigin(0.5f, 0f)
-                )
-        ) {
-            options.forEachIndexed { index, action ->
-                DropdownMenuItem(modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 8.dp)
-                    .clip(RoundedCornerShape(12.dp)), onClick = {
-                    action.onClick()
-                    isExpanded = false
-                }, text = {
-                    Text(
-                        text = stringResource(action.description),
-                        style = MaterialTheme.typography.bodyMedium
-                    )
-                }, leadingIcon = {
-                    when (val iconSource = action.icon) {
-                        is ImageVector -> Icon(
-                            imageVector = iconSource,
-                            contentDescription = stringResource(action.description),
-                            modifier = Modifier.padding(end = 8.dp),
-                            tint = MaterialTheme.colorScheme.primary.copy(alpha = 0.8f)
-                        )
-
-                        is Int -> Icon(
-                            painter = painterResource(id = iconSource),
-                            contentDescription = stringResource(action.description),
-                            modifier = Modifier.padding(end = 8.dp),
-                            tint = MaterialTheme.colorScheme.primary.copy(alpha = 0.8f)
-                        )
-
-                        else -> throw IllegalArgumentException("Icon must be either a drawable resource ID or an ImageVector")
-                    }
-                })
-                if (index < options.lastIndex) {
-                    HorizontalDivider(
-                        modifier = Modifier
-                            .padding(horizontal = 24.dp)
-                            .alpha(0.5f)
-                    )
-                }
-            }
-        }
+            options = options
+        )
     }
 }
 
