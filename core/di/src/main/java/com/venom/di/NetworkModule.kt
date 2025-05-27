c2a4411d6880ef05cbd4d8b773979d4ff4bd2139
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2025-05-27 19:29:46 +0300

    Configure network module with ChatGPT and Gemini API services.

--- core/di/src/main/java/com/venom/di/NetworkModule.kt
+++ core/di/src/main/java/com/venom/di/NetworkModule.kt
@@ -1,5 +1,7 @@
 package com.venom.di
 
+import com.venom.data.api.ChatGPTService
+import com.venom.data.api.GeminiService
 import com.venom.data.api.GithubApi
 import com.venom.data.api.OcrService
 import com.venom.data.api.TranslationService
@@ -11,6 +13,7 @@ import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 import javax.inject.Named
 import javax.inject.Singleton
 
@@ -35,14 +38,26 @@ object NetworkModule {
 
     @Provides
     @Singleton
+    @Named("RegularOkHttpClient")
     fun provideOkHttpClient(): OkHttpClient {
         return OkHttpClient.Builder().build()
     }
 
     @Provides
     @Singleton
+    @Named("AiOkHttpClient")
+    fun provideAiOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .writeTimeout(30, TimeUnit.SECONDS)
+            .build()
+    }
+
+    @Provides
+    @Singleton
     @Named("OcrRetrofit")
-    fun provideOcrRetrofit(okHttpClient: OkHttpClient): Retrofit {
+    fun provideOcrRetrofit(@Named("RegularOkHttpClient") okHttpClient: OkHttpClient): Retrofit {
         return Retrofit.Builder()
             .baseUrl(OcrService.BASE_URL)
             .client(okHttpClient)
@@ -53,11 +68,45 @@ object NetworkModule {
     @Provides
     @Singleton
     @Named("TranslationRetrofit")
-    fun provideTranslationRetrofit(okHttpClient: OkHttpClient): Retrofit {
+    fun provideTranslationRetrofit(@Named("RegularOkHttpClient") okHttpClient: OkHttpClient): Retrofit {
         return Retrofit.Builder()
             .baseUrl(TranslationService.BASE_URL)
             .client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
+    }
+
+    @Provides
+    @Singleton
+    @Named("ChatGPTRetrofit")
+    fun provideChatGPTRetrofit(@Named("AiOkHttpClient") aiOkHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(ChatGPTService.BASE_URL)
+            .client(aiOkHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    @Named("GeminiRetrofit")
+    fun provideGeminiRetrofit(@Named("AiOkHttpClient") aiOkHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(GeminiService.BASE_URL)
+            .client(aiOkHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideChatGPTService(@Named("ChatGPTRetrofit") retrofit: Retrofit): ChatGPTService {
+        return retrofit.create(ChatGPTService::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideGeminiService(@Named("GeminiRetrofit") retrofit: Retrofit): GeminiService {
+        return retrofit.create(GeminiService::class.java)
     }
 }
