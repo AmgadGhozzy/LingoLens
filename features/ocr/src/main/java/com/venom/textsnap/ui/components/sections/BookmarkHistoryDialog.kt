f2ef89018a346db26677717501f4c7051e6bccef
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2025-05-27 21:07:43 +0300

    Removes unused settings and navigation related files and components.

--- features/ocr/src/main/java/com/venom/textsnap/ui/components/sections/BookmarkHistoryDialog.kt
+++
@@ -1,9 +0,0 @@
-package com.venom.textsnap.ui.components.sections
-
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-
-@Composable
-fun BookmarkHistoryDialog(modifier: Modifier = Modifier) {
-    
-}
