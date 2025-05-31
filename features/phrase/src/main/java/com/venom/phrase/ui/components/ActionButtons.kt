e8e6255b4deb6893d6400e69de018e566f88cf65
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2025-06-01 02:12:40 +0300

    Refactors ActionButtons to use CustomFilledIconButton for share action.

--- features/phrase/src/main/java/com/venom/phrase/ui/components/ActionButtons.kt
+++ features/phrase/src/main/java/com/venom/phrase/ui/components/ActionButtons.kt
@@ -3,15 +3,15 @@ package com.venom.phrase.ui.components
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import com.venom.resources.R
 import com.venom.ui.components.buttons.CopyButton
-import com.venom.ui.components.buttons.CustomButton
+import com.venom.ui.components.buttons.CustomFilledIconButton
 
 @Composable
 fun ActionButtons(
@@ -23,11 +23,15 @@ fun ActionButtons(
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.End
     ) {
-        CopyButton(onClick = onCopyClick, iconSize = 28.dp)
+        CopyButton(onClick = onCopyClick)
 
-        CustomButton(
+        CustomFilledIconButton(
             icon = R.drawable.icon_share,
             onClick = onShareClick,
+            colors = IconButtonDefaults.filledIconButtonColors(
+                containerColor = MaterialTheme.colorScheme.primaryContainer.copy(alpha = 0.3f),
+                contentColor = MaterialTheme.colorScheme.primary
+            ),
             contentDescription = stringResource(R.string.action_share)
         )
     }
