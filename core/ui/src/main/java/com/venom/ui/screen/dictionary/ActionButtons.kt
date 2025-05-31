bba40be1edf9e829cc51e9109ecfaaf9e436ef11
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2025-06-01 02:10:50 +0300

    Refactors ActionButtons to use icon buttons and filled speech button.

--- core/ui/src/main/java/com/venom/ui/screen/dictionary/ActionButtons.kt
+++ core/ui/src/main/java/com/venom/ui/screen/dictionary/ActionButtons.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -12,34 +14,39 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.venom.resources.R
 import com.venom.ui.components.buttons.CopyButton
-import com.venom.ui.components.buttons.CustomButton
+import com.venom.ui.components.buttons.CustomFilledIconButton
+import com.venom.ui.components.buttons.SpeechFilledButton
 
 @Composable
 fun ActionButtons(
+    modifier: Modifier = Modifier,
     text: String,
     onSpeak: (String) -> Unit,
     onCopy: (String) -> Unit,
     onShare: ((String) -> Unit),
     isSpeaking: Boolean = false,
-    modifier: Modifier = Modifier
 ) {
     Row(
         modifier = modifier.fillMaxWidth(),
         horizontalArrangement = Arrangement.End,
         verticalAlignment = Alignment.CenterVertically
     ) {
-        CustomButton(
-            onClick = { onSpeak(text) },
-            icon = if (isSpeaking) R.drawable.icon_record else R.drawable.icon_sound,
-            contentDescription = stringResource(R.string.action_speak)
-        )
+
+        CopyButton(onClick = { onCopy(text) })
         Spacer(modifier = Modifier.width(8.dp))
-        CustomButton(
-            onClick = { onShare(text) },
+        CustomFilledIconButton(
             icon = R.drawable.icon_share,
+            onClick = { onShare(text) },
+            colors = IconButtonDefaults.filledIconButtonColors(
+                containerColor = MaterialTheme.colorScheme.primaryContainer.copy(alpha = 0.3f),
+                contentColor = MaterialTheme.colorScheme.primary
+            ),
             contentDescription = stringResource(R.string.action_share)
         )
         Spacer(modifier = Modifier.width(8.dp))
-        CopyButton(onClick = { onCopy(text) })
+        SpeechFilledButton(
+            onSpeakClick = { onSpeak(text) },
+            isSpeaking = isSpeaking
+        )
     }
 }
