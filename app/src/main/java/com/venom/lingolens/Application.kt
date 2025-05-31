7f3a16ff4cfbc500c4f3eedbf7336c0c1fb2eec2
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2025-06-01 02:09:46 +0300

    Enable StrictMode in debug builds to detect potential issues.

--- app/src/main/java/com/venom/lingolens/Application.kt
+++ app/src/main/java/com/venom/lingolens/Application.kt
@@ -1,6 +1,8 @@
 package com.venom.lingolens
 
 import android.app.Application
+import android.os.StrictMode
+import android.util.Log
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
@@ -11,5 +13,28 @@ class Application : Application() {
     override fun onCreate() {
         super.onCreate()
         adsManager.initialize(this)
+        if (BuildConfig.DEBUG) {
+            enableStrictMode()
+        }
+    }
+
+    private fun enableStrictMode() {
+        Log.d("StrictMode", "StrictMode Enabled in DEBUG mode")
+
+        StrictMode.setThreadPolicy(
+            StrictMode.ThreadPolicy.Builder()
+                .detectAll()
+                .penaltyLog()
+                .penaltyFlashScreen()
+                .build()
+        )
+
+        StrictMode.setVmPolicy(
+            StrictMode.VmPolicy.Builder()
+                .detectLeakedSqlLiteObjects()
+                .detectLeakedClosableObjects()
+                .penaltyLog()
+                .build()
+        )
     }
 }
