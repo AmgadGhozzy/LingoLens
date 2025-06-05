fced3eb583c1aef56f2039f1f35dcc17c4995c6c
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2025-06-05 23:43:51 +0300

    Refactors TranslatedTextSection to use AnimatedResponseTextField.

--- features/translation/src/main/java/com/venom/lingopro/ui/components/sections/TranslatedTextSection.kt
+++ features/translation/src/main/java/com/venom/lingopro/ui/components/sections/TranslatedTextSection.kt
@@ -1,7 +1,7 @@
 package com.venom.lingopro.ui.components.sections
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -10,24 +10,16 @@ import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import com.venom.resources.R
 import com.venom.ui.components.inputs.AnimatedResponseTextField
-import com.venom.ui.components.inputs.CustomTextField
 
-/**
- * A composable function that displays a translated text. The text is displayed in a [CustomTextField]
- * which is readonly and has a placeholder text.
- *
- * @param translatedTextValue the translated text to be displayed
- * @param modifier a modifier that can be used to customize the appearance of the component
- */
 @Composable
 fun TranslatedTextSection(
     translatedTextValue: TextFieldValue,
-    modifier: Modifier = Modifier,
+    modifier: Modifier = Modifier
 ) {
     AnimatedResponseTextField(
         text = translatedTextValue.text,
         placeHolderText = stringResource(R.string.translate_result_placeholder),
-        modifier = Modifier
+        modifier = modifier
             .fillMaxWidth()
             .background(MaterialTheme.colorScheme.surfaceContainer)
     )
@@ -35,10 +27,8 @@ fun TranslatedTextSection(
 
 @Preview(showBackground = true)
 @Composable
-fun TranslatedTextSectionPreview() {
+private fun TranslatedTextSectionPreview() {
     MaterialTheme {
-        TranslatedTextSection(
-            translatedTextValue = TextFieldValue("")
-        )
+        TranslatedTextSection(translatedTextValue = TextFieldValue(""))
     }
 }
