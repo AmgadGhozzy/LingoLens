6a41804334ec3b30ad72491ceaaa992a45e02b1d
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2025-06-05 23:29:58 +0300

    Refactors TranslationRepository to use operations interfaces.

--- core/di/src/main/java/com/venom/di/TranslationModule.kt
+++ core/di/src/main/java/com/venom/di/TranslationModule.kt
@@ -7,6 +7,12 @@ import com.venom.data.api.GroqService
 import com.venom.data.api.HuggingFaceService
 import com.venom.data.api.TranslationService
 import com.venom.data.local.dao.TranslationDao
+import com.venom.data.repo.OfflineTranslationOperations
+import com.venom.data.repo.OfflineTranslationRepository
+import com.venom.data.repo.OnlineTranslationOperations
+import com.venom.data.repo.OnlineTranslationRepository
+import com.venom.data.repo.TranslationHistoryOperations
+import com.venom.data.repo.TranslationHistoryRepository
 import com.venom.data.repo.TranslationRepository
 import dagger.Module
 import dagger.Provides
@@ -20,21 +26,40 @@ object TranslationModule {
 
     @Provides
     @Singleton
-    fun provideTranslationRepository(
+    fun provideOnlineTranslationOperations(
         translationService: TranslationService,
         chatGPTService: ChatGPTService,
         geminiService: GeminiService,
         groqService: GroqService,
         deepSeekService: DeepSeekService,
-        huggingFaceService: HuggingFaceService,
-        translationDao: TranslationDao
-    ): TranslationRepository = TranslationRepository(
+        huggingFaceService: HuggingFaceService
+    ): OnlineTranslationOperations = OnlineTranslationRepository(
         translationService,
         chatGPTService,
         geminiService,
         groqService,
         deepSeekService,
-        huggingFaceService,
-        translationDao
+        huggingFaceService
+    )
+
+    @Provides
+    @Singleton
+    fun provideOfflineTranslationOperations(): OfflineTranslationOperations = OfflineTranslationRepository()
+
+    @Provides
+    @Singleton
+    fun provideTranslationHistoryOperations(translationDao: TranslationDao): TranslationHistoryOperations = 
+        TranslationHistoryRepository(translationDao)
+
+    @Provides
+    @Singleton
+    fun provideTranslationRepository(
+        onlineTranslationOperations: OnlineTranslationOperations,
+        offlineTranslationOperations: OfflineTranslationOperations,
+        translationHistoryOperations: TranslationHistoryOperations
+    ): TranslationRepository = TranslationRepository(
+        onlineTranslationOperations,
+        offlineTranslationOperations,
+        translationHistoryOperations
     )
 }
