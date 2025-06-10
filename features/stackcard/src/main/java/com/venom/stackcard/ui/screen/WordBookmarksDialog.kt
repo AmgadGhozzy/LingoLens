a05f724a46e3ddf1603d47dee99b3dab78015f4d
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2025-06-10 23:56:37 +0300

    Implements copy and speak functionalities in bookmarks screen.

--- features/stackcard/src/main/java/com/venom/stackcard/ui/screen/WordBookmarksDialog.kt
+++ features/stackcard/src/main/java/com/venom/stackcard/ui/screen/WordBookmarksDialog.kt
@@ -2,22 +2,28 @@ package com.venom.stackcard.ui.screen
 
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.venom.ui.viewmodel.TTSViewModel
+import com.venom.utils.Extensions.copyToClipboard
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun WordBookmarksDialog(
-    onDismiss: () -> Unit
+    onDismiss: () -> Unit,
+    ttsViewModel: TTSViewModel = hiltViewModel()
 ) {
+    val context = LocalContext.current
     Dialog(
         onDismissRequest = onDismiss, properties = DialogProperties(
             usePlatformDefaultWidth = false, decorFitsSystemWindows = false
         )
     ) {
         BookmarksScreen(
-            onCopy = {},
-            onSpeak = {},
+            onCopy = { context.copyToClipboard(it.englishEn) },
+            onSpeak = { ttsViewModel.speak(it.englishEn) },
             onBackClick = onDismiss,
         )
     }
