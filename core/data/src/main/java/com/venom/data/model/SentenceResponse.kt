531ce8269cd7d70887abb92b0a07105ad951ac02
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2025-06-13 23:54:59 +0300

    feat(data): Define SentenceResponse and NetworkResult
    Introduces the `SentenceResponse` data class to model API
    responses for sentences. This class includes fields for
    the word, a list of sentences, an optional image URL,
    a list of sources, and the total number of sentences.
    
    Also defines a sealed class `NetworkResult` to represent
    the different states of a network request: `Success`,
    `Error`, and `Loading`.

---
+++ core/data/src/main/java/com/venom/data/model/SentenceResponse.kt
@@ -0,0 +1,18 @@
+package com.venom.data.model
+
+import com.google.gson.annotations.SerializedName
+
+data class SentenceResponse(
+    val word: String,
+    val sentences: List<String>,
+    val imageUrl: String?,
+    val sources: List<String>,
+    @SerializedName("total_sentences")
+    val totalSentences: Int
+)
+
+sealed class NetworkResult<T> {
+    data class Success<T>(val data: T) : NetworkResult<T>()
+    data class Error<T>(val message: String) : NetworkResult<T>()
+    data class Loading<T>(val isLoading: Boolean = true) : NetworkResult<T>()
+}
