2a765f5acc30c8190e7c872e0e1f7a1362eba17f
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2025-06-13 23:53:38 +0300

    Allow DynamicStyledText to accept AnnotatedString or String as text.

--- core/ui/src/main/java/com/venom/ui/components/common/DynamicStyledText.kt
+++ core/ui/src/main/java/com/venom/ui/components/common/DynamicStyledText.kt
@@ -9,43 +9,77 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.venom.utils.getTextDirection
 
 @Composable
 fun DynamicStyledText(
-    text: String,
+    text: Any,
     modifier: Modifier = Modifier,
     textAlign: TextAlign = TextAlign.Unspecified,
     minFontSize: Int = 22,
     maxFontSize: Int = 32,
     lineHeight: Int = 32,
+    maxLines: Int = Int.MAX_VALUE,
     fontWeight: FontWeight = FontWeight.Bold,
     color: Color = MaterialTheme.colorScheme.onBackground,
 ) {
-    val dynamicFontSize = (maxFontSize - (text.length / 5)).coerceAtLeast(minFontSize)
+
+    val dynamicFontSize = (maxFontSize - (text.toString().count() / 5)).coerceAtLeast(minFontSize)
 
     SelectionContainer {
-        Text(
-            text = text,
-            textAlign = textAlign,
-            style = TextStyle(
-                fontSize = dynamicFontSize.sp,
-                color = color,
-                lineHeight = lineHeight.sp,
-                fontWeight = fontWeight,
-                textDirection = getTextDirection(text),
-                shadow = Shadow(
-                    color = MaterialTheme.colorScheme.onBackground.copy(alpha = 0.2f),
-                    offset = Offset(2f, 2f),
-                    blurRadius = 4f
+        when (text) {
+            is AnnotatedString -> {
+                Text(
+                    text = text,
+                    textAlign = textAlign,
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = maxLines,
+                    style = TextStyle(
+                        fontSize = dynamicFontSize.sp,
+                        color = color,
+                        lineHeight = lineHeight.sp,
+                        fontWeight = fontWeight,
+                        textDirection = getTextDirection(text.text),
+                        shadow = Shadow(
+                            color = MaterialTheme.colorScheme.onBackground.copy(alpha = 0.2f),
+                            offset = Offset(2f, 2f),
+                            blurRadius = 4f
+                        )
+                    ),
+                    modifier = modifier.padding(8.dp)
                 )
-            ),
-            modifier = modifier.padding(8.dp)
-        )
+            }
+
+            is String -> {
+                Text(
+                    text = text,
+                    textAlign = textAlign,
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = maxLines,
+                    style = TextStyle(
+                        fontSize = dynamicFontSize.sp,
+                        color = color,
+                        lineHeight = lineHeight.sp,
+                        fontWeight = fontWeight,
+                        textDirection = getTextDirection(text),
+                        shadow = Shadow(
+                            color = MaterialTheme.colorScheme.onBackground.copy(alpha = 0.2f),
+                            offset = Offset(2f, 2f),
+                            blurRadius = 4f
+                        )
+                    ),
+                    modifier = modifier.padding(8.dp)
+                )
+            }
+
+            else -> {}
+        }
     }
 }
