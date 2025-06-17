afde560cde593b62a7b5d12649d92a887d7c49cc
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2025-06-17 23:48:17 +0300

    Configure DialogDatabase to not export schema.

--- features/dialog/src/main/java/com/venom/dialog/data/local/DialogDatabase.kt
+++ features/dialog/src/main/java/com/venom/dialog/data/local/DialogDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.RoomDatabase
 import com.venom.dialog.data.local.dao.DialogMessageDao
 import com.venom.dialog.data.local.model.DialogMessage
 
-@Database(entities = [DialogMessage::class], version = 1)
+@Database(entities = [DialogMessage::class], version = 1, exportSchema = false)
 abstract class DialogDatabase : RoomDatabase() {
     abstract fun dialogMessageDao(): DialogMessageDao
 }
