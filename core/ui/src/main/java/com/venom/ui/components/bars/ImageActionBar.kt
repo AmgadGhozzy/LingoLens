37c195d5bcef49e9ba52680fadb597a7268a89d2
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2025-06-17 23:41:21 +0300

    Refactors ImageActionBar to improve layout and conditionally render OverflowMenu.

--- core/ui/src/main/java/com/venom/ui/components/bars/ImageActionBar.kt
+++ core/ui/src/main/java/com/venom/ui/components/bars/ImageActionBar.kt
@@ -1,8 +1,6 @@
 package com.venom.ui.components.bars
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -10,9 +8,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.venom.resources.R
 import com.venom.ui.components.buttons.CustomFilledIconButton
 import com.venom.ui.components.common.ActionItem
 import com.venom.ui.components.menus.OverflowMenu
@@ -27,16 +23,17 @@ fun ImageActionBar(
     Surface(
         shape = MaterialTheme.shapes.extraLarge,
         color = backgroundColor,
-        modifier = modifier.fillMaxWidth()
+        modifier = modifier
     ) {
         Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(vertical = 8.dp),
+            modifier = Modifier.padding(vertical = 8.dp),
             horizontalArrangement = Arrangement.SpaceEvenly,
             verticalAlignment = Alignment.CenterVertically
         ) {
-            if (actions.size > maxActionsVisible) OverflowMenu(actions.drop(maxActionsVisible))
+            if (actions.size > maxActionsVisible) {
+                OverflowMenu(actions.drop(maxActionsVisible))
+            }
+
             actions.take(maxActionsVisible).forEach { action ->
                 CustomFilledIconButton(
                     icon = action.icon,
@@ -49,32 +46,4 @@ fun ImageActionBar(
             }
         }
     }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun ImageActionBarPreview() {
-    ImageActionBar(
-        actions = listOf(
-            ActionItem.Action(
-                icon = R.drawable.ic_paragraph_on,
-                onClick = {},
-                description = R.string.action_hide_paragraphs,
-            ), ActionItem.Action(
-                icon = R.drawable.ic_labels_shown,
-                onClick = {},
-                description = R.string.action_hide_labels,
-                enabled = true
-            ), ActionItem.Action(
-                icon = R.drawable.ic_select_all,
-                onClick = {},
-                description = R.string.select_language,
-                enabled = false
-            ), ActionItem.Action(
-                icon = R.drawable.icon_translate,
-                onClick = {},
-                description = R.string.action_translate,
-            )
-        )
-    )
 }
