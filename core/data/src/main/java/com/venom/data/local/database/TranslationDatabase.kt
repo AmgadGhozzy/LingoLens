3b166bff9b5d05127914c2ade8e48d4801999bd7
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2025-06-21 23:53:09 +0300

    Add TranslationDatabase Room database with TranslationDao and type converters.

---
+++ core/data/src/main/java/com/venom/data/local/database/TranslationDatabase.kt
@@ -0,0 +1,14 @@
+package com.venom.data.local.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.venom.data.local.converters.StringListConverter
+import com.venom.data.local.dao.TranslationDao
+import com.venom.data.model.TranslationEntity
+
+@Database(entities = [TranslationEntity::class], version = 1, exportSchema = false)
+@TypeConverters(StringListConverter::class)
+abstract class TranslationDatabase : RoomDatabase() {
+    abstract fun translationDao(): TranslationDao
+}
