47fa3583b42ce8d73f2167ee03ac53c94047e5c7
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2025-06-21 23:51:48 +0300

    Rename TranslationService to GoogleTranslateService for clarity.

--- core/data/src/main/java/com/venom/data/api/GoogleTranslateService.kt
+++ core/data/src/main/java/com/venom/data/api/GoogleTranslateService.kt
@@ -1,19 +1,19 @@
 package com.venom.data.api
 
-import com.venom.data.model.TranslationResponse
+import com.venom.data.model.GoogleTranslationResponse
 import retrofit2.http.GET
 import retrofit2.http.Query
 
-interface TranslationService {
+interface GoogleTranslateService {
     @GET("translate_a/single?dt=t&dt=at&dt=bd&dt=ex&dt=md&dt=rw&dt=ss&dt=rm")
-    suspend fun getTranslation(
-        @Query("client") client: String = "gtx",
+    suspend fun translateWithGoogle(
         @Query("sl") sourceLanguage: String,
         @Query("tl") targetLanguage: String,
         @Query("q") query: String,
+        @Query("client") client: String = "gtx",
         @Query("dj") dj: Int = 1,
         @Query("dt") dt: List<String> = listOf("t", "at", "bd", "ex", "md", "rw", "ss", "rm"),
-    ): TranslationResponse
+    ): GoogleTranslationResponse
 
     companion object {
         const val BASE_URL = "https://translate.googleapis.com/"
