56d8541ea449ae623351b10f174ea24c26f8bf27
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2025-06-22 00:00:56 +0300

    Refactors WordDatabase to use the ListStringConverter from the `converters` package.

--- features/stackcard/src/main/java/com/venom/stackcard/data/local/WordDatabase.kt
+++ features/stackcard/src/main/java/com/venom/stackcard/data/local/WordDatabase.kt
@@ -3,7 +3,7 @@ package com.venom.stackcard.data.local
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
-import com.venom.data.local.typeconverter.ListStringConverter
+import com.venom.data.local.converters.ListStringConverter
 import com.venom.stackcard.data.local.dao.WordDao
 import com.venom.stackcard.data.model.WordEntity
 
