1bab51d535f54b61ab48868a2286c4995a612584
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2025-07-19 03:43:48 +0300

    Renames 'text' parameter to 'sourceText' in the translate function for clarity.

--- core/domain/src/main/java/com/venom/domain/repo/ITranslationRepository.kt
+++ core/domain/src/main/java/com/venom/domain/repo/ITranslationRepository.kt
@@ -5,7 +5,7 @@ import kotlinx.coroutines.flow.Flow
 
 interface ITranslationRepository {
     suspend fun translate(
-        text: String,
+        sourceText: String,
         sourceLang: String = "auto",
         targetLang: String = "ar",
         providerId: String,
