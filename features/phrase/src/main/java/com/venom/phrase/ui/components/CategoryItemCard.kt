b31c43f09c713d03c4eae7aa2baff59c459c497d
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2025-08-09 22:40:01 +0300

    Refactor CategoryItemCard to use GlassCard and add content size animation.

--- features/phrase/src/main/java/com/venom/phrase/ui/components/CategoryItemCard.kt
+++ features/phrase/src/main/java/com/venom/phrase/ui/components/CategoryItemCard.kt
@@ -1,16 +1,15 @@
 package com.venom.phrase.ui.components
 
 import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.ChevronRight
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.MaterialTheme
@@ -28,23 +27,19 @@ import com.venom.phrase.ui.viewmodel.PhraseUiState
 import com.venom.resources.R
 import com.venom.ui.components.buttons.CustomFilledIconButton
 import com.venom.ui.components.common.DynamicStyledText
+import com.venom.ui.components.other.GlassCard
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CategoryItemCard(
     state: PhraseUiState, category: Category, onClick: () -> Unit
 ) {
-    Card(
+    GlassCard(
         onClick = onClick,
         modifier = Modifier
             .fillMaxWidth()
-            .animateContentSize()
-            .semantics { contentDescription = "Category card: ${category.englishEn}" },
-        shape = RoundedCornerShape(24.dp),
-        colors = CardDefaults.cardColors(
-            containerColor = MaterialTheme.colorScheme.secondaryContainer.copy(alpha = 0.5f)
-        ),
-        elevation = CardDefaults.cardElevation(defaultElevation = 0.dp)
+            .semantics { contentDescription = "Category card: ${category.englishEn}" }
+            .animateContentSize(spring(Spring.DampingRatioMediumBouncy, Spring.StiffnessLow))
     ) {
         Row(
             modifier = Modifier
@@ -53,7 +48,7 @@ fun CategoryItemCard(
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.spacedBy(16.dp)
         ) {
-            CategoryIcon(category.icon.toString())
+            CategoryIcon(category.icon)
             Column(modifier = Modifier.weight(1f)) {
                 DynamicStyledText(
                     category.getTranslation(state.sourceLang.code), modifier = Modifier
