84ec76fb1736e8d95500a40761281a17a00ab719
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2025-08-09 22:38:16 +0300

    Improves PhrasebookTopBar appearance by adding bookmark button and adjusting language bar style.

--- features/phrase/src/main/java/com/venom/phrase/ui/components/PhrasebookTopBar.kt
+++ features/phrase/src/main/java/com/venom/phrase/ui/components/PhrasebookTopBar.kt
@@ -1,6 +1,7 @@
 package com.venom.phrase.ui.components
 
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -11,8 +12,12 @@ import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import com.venom.resources.R
+import com.venom.ui.components.buttons.CustomButton
 import com.venom.ui.screen.langselector.LangSelectorViewModel
 import com.venom.ui.screen.langselector.LanguageBar
 
@@ -26,17 +31,18 @@ fun PhrasebookTopBar(
     modifier: Modifier = Modifier
 ) {
     TopAppBar(
-        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
-        scrollBehavior = scrollBehavior,
         title = {
             PhrasebookTitle(
                 totalPhrases = totalPhrases
             )
         },
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        scrollBehavior = scrollBehavior,
         actions = {
             LanguageBar(
                 viewModel = viewModel,
                 modifier = Modifier
+                    .height(56.dp)
                     .width(256.dp)
                     .clip(RoundedCornerShape(28.dp))
                     .border(
@@ -45,15 +51,17 @@ fun PhrasebookTopBar(
                         shape = RoundedCornerShape(28.dp)
                     )
             )
-//            CustomButton(
-//                icon = R.drawable.icon_bookmark_filled,
-//                contentDescription = stringResource(R.string.bookmarks_title),
-//                onClick = onBookmarkClick
-//            )
+            CustomButton(
+                icon = R.drawable.icon_bookmark_filled,
+                contentDescription = stringResource(R.string.bookmarks_title),
+                onClick = onBookmarkClick,
+                iconSize = 40.dp,
+                showBorder = false
+            )
         },
         colors = TopAppBarDefaults.topAppBarColors(
-            containerColor = MaterialTheme.colorScheme.surface,
-            scrolledContainerColor = MaterialTheme.colorScheme.surface
+            containerColor = Color.Transparent,
+            scrolledContainerColor =Color.Transparent
         )
     )
 }
