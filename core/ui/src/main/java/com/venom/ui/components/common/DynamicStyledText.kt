c8d7e6450694dd1e16f3275c8d54160d3eb3070c
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2025-08-29 19:29:06 +0300

    Refactors DynamicStyledText to use MaterialTheme typography and reduce max font size.

--- core/ui/src/main/java/com/venom/ui/components/common/DynamicStyledText.kt
+++ core/ui/src/main/java/com/venom/ui/components/common/DynamicStyledText.kt
@@ -10,7 +10,6 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shadow
 import androidx.compose.ui.text.AnnotatedString
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
@@ -24,14 +23,28 @@ fun DynamicStyledText(
     modifier: Modifier = Modifier,
     textAlign: TextAlign = TextAlign.Unspecified,
     minFontSize: Int = 22,
-    maxFontSize: Int = 32,
-    lineHeight: Int = 32,
+    maxFontSize: Int = 30,
+    lineHeight: Int = 30,
     maxLines: Int = Int.MAX_VALUE,
     fontWeight: FontWeight = FontWeight.Bold,
     color: Color = MaterialTheme.colorScheme.onBackground,
 ) {
 
     val dynamicFontSize = (maxFontSize - (text.toString().count() / 5)).coerceAtLeast(minFontSize)
+
+    val baseStyle = MaterialTheme.typography.headlineMedium
+    val customStyle = baseStyle.copy(
+        fontSize = dynamicFontSize.sp,
+        color = color,
+        lineHeight = lineHeight.sp,
+        fontWeight = fontWeight,
+        textDirection = getTextDirection(text.toString()),
+        shadow = Shadow(
+            color = MaterialTheme.colorScheme.onBackground.copy(0.2f),
+            offset = Offset(2f, 2f),
+            blurRadius = 4f
+        )
+    )
 
     SelectionContainer {
         when (text) {
@@ -41,18 +54,7 @@ fun DynamicStyledText(
                     textAlign = textAlign,
                     overflow = TextOverflow.Ellipsis,
                     maxLines = maxLines,
-                    style = TextStyle(
-                        fontSize = dynamicFontSize.sp,
-                        color = color,
-                        lineHeight = lineHeight.sp,
-                        fontWeight = fontWeight,
-                        textDirection = getTextDirection(text.text),
-                        shadow = Shadow(
-                            color = MaterialTheme.colorScheme.onBackground.copy(alpha = 0.2f),
-                            offset = Offset(2f, 2f),
-                            blurRadius = 4f
-                        )
-                    ),
+                    style = customStyle,
                     modifier = modifier.padding(8.dp)
                 )
             }
@@ -63,18 +65,7 @@ fun DynamicStyledText(
                     textAlign = textAlign,
                     overflow = TextOverflow.Ellipsis,
                     maxLines = maxLines,
-                    style = TextStyle(
-                        fontSize = dynamicFontSize.sp,
-                        color = color,
-                        lineHeight = lineHeight.sp,
-                        fontWeight = fontWeight,
-                        textDirection = getTextDirection(text),
-                        shadow = Shadow(
-                            color = MaterialTheme.colorScheme.onBackground.copy(alpha = 0.2f),
-                            offset = Offset(2f, 2f),
-                            blurRadius = 4f
-                        )
-                    ),
+                    style = customStyle,
                     modifier = modifier.padding(8.dp)
                 )
             }
