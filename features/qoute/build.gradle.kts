a51454a707335e797e88431bb1bd394670729aba
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2025-09-10 21:14:41 +0300

    Implements and integrates the 'qoute' feature module with necessary configurations and dependencies.

---
+++ features/qoute/build.gradle.kts
@@ -0,0 +1,58 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.hilt)
+    id("kotlin-kapt")
+}
+
+android {
+    namespace = "com.venom.qoute"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 24
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            isShrinkResources = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+
+    // Core Modules
+    implementation(project(":core:data"))
+    implementation(project(":core:di"))
+    implementation(project(":core:resources"))
+    implementation(project(":core:ui"))
+    implementation(project(":features:phrase"))
+
+    implementation(libs.androidx.datastore)
+    implementation(libs.androidx.datastore.preferences)
+
+    // Hilt
+    api(libs.hilt.android)
+    implementation(libs.androidx.activity.ktx)
+    kapt(libs.hilt.android.compiler)
+    api(libs.hilt.navigation.compose)
+}
