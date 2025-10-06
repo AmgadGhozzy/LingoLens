ac61dcbefae84a57467bd06853dfe99eaa744362
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2025-10-06 23:12:52 +0300

    Refactors database entities by moving them to a dedicated 'entity' package.

--- core/data/src/main/java/com/venom/data/local/database/OcrDatabase.kt
+++ core/data/src/main/java/com/venom/data/local/database/OcrDatabase.kt
@@ -3,9 +3,9 @@ package com.venom.data.local.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
-import com.venom.data.local.Entity.OcrEntity
 import com.venom.data.local.converters.OcrResponseConverter
 import com.venom.data.local.dao.OcrDao
+import com.venom.data.local.entity.OcrEntity
 
 @Database(entities = [OcrEntity::class], version = 1, exportSchema = false)
 @TypeConverters(OcrResponseConverter::class)
