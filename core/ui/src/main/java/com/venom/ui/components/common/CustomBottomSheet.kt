0189aba0b92fba955d1f9e6ef57ff753e9542591
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2025-12-26 07:05:14 +0200

    Updates CustomBottomSheet

--- core/ui/src/main/java/com/venom/ui/components/common/CustomBottomSheet.kt
+++ core/ui/src/main/java/com/venom/ui/components/common/CustomBottomSheet.kt
@@ -5,8 +5,11 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.rememberModalBottomSheetState
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.unit.dp
@@ -32,7 +35,7 @@ fun CustomBottomSheet(
         sheetState = sheetState,
         onDismissRequest = onDismiss,
         tonalElevation = 2.dp,
-        containerColor = MaterialTheme.colorScheme.surfaceContainerLow,
+        containerColor = MaterialTheme.colorScheme.surfaceContainerLowest,
         shape = RoundedCornerShape(topStart = 28.dp, topEnd = 28.dp),
         content = content
     )
