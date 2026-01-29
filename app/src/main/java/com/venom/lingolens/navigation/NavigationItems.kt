3b328213c24df491506e875fb1ee17dd2446c528
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2026-01-29 20:04:02 +0200

    Update NavigationItems for icons and visibility

--- app/src/main/java/com/venom/lingolens/navigation/NavigationItems.kt
+++ app/src/main/java/com/venom/lingolens/navigation/NavigationItems.kt
@@ -10,10 +10,10 @@ enum class NavigationItems(
     val showInBottomBar: Boolean = true
 ) {
     DIALOG(Screen.Dialog, R.drawable.icon_dialogs_activated, R.string.dialog_title, false),
-    QUOTE(Screen.Quote, R.drawable.icon_quotes, R.string.quote_title),
+    QUOTE(Screen.Quote, R.drawable.ic_quotes_fill, R.string.quote_title),
     PHRASE(Screen.Phrases, R.drawable.icon_dialog, R.string.phrase_title),
-    QUIZ(Screen.Quiz, R.drawable.quiz_logo, R.string.quiz_title),
     WORDCARD(Screen.StackCard, R.drawable.icon_cards1, R.string.stackcard_title),
-    OCR(Screen.Ocr, R.drawable.icon_camera, R.string.ocr_title),
+    QUIZ(Screen.Quiz, R.drawable.ic_question_fill, R.string.quiz_title),
+    OCR(Screen.Ocr, R.drawable.icon_camera, R.string.ocr_title, false),
     TRANSLATE(Screen.Translation, R.drawable.icon_translate, R.string.translate_title);
 }
