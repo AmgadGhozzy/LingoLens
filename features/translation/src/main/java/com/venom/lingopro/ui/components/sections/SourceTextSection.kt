a3e0a82d1641acab350bae833f9bf880289a6218
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2026-02-02 22:54:29 +0200

    Adjust SourceTextSection imports and semantics

--- features/translation/src/main/java/com/venom/lingopro/ui/components/sections/SourceTextSection.kt
+++ features/translation/src/main/java/com/venom/lingopro/ui/components/sections/SourceTextSection.kt
@@ -1,5 +1,6 @@
 package com.venom.lingopro.ui.components.sections
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
@@ -9,32 +10,22 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.LiveRegionMode
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.liveRegion
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.input.TextFieldValue
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.venom.resources.R
 import com.venom.ui.components.buttons.CustomButton
 import com.venom.ui.components.common.PulseAnimation
+import com.venom.ui.components.common.adp
 import com.venom.ui.components.inputs.CustomTextField
 
-/**
- * SourceTextSection is a composable function that displays a text input field that can be used
- * to enter text to be translated. It also displays a loading indicator if a translation is in
- * progress and a clear button to clear the input text.
- *
- * @param sourceTextValue the current text in the input field
- * @param onTextChange a callback that is called when the text in the input field changes
- * @param onClearText a callback that is called when the clear button is clicked
- * @param isLoading a boolean that indicates if a translation is in progress
- * @param modifier a modifier that can be used to customize the appearance of the component
- */
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SourceTextSection(
     sourceTextValue: TextFieldValue,
@@ -48,7 +39,9 @@ fun SourceTextSection(
         horizontalArrangement = Arrangement.SpaceBetween,
         modifier = modifier
             .fillMaxWidth()
-
+            .semantics {
+                contentDescription = "Source text input area"
+            }
     ) {
         CustomTextField(
             textValue = sourceTextValue,
@@ -56,41 +49,45 @@ fun SourceTextSection(
             placeHolderText = stringResource(R.string.translate_type_text),
             modifier = Modifier
                 .weight(1f)
-                .padding(vertical = 12.dp)
+                .padding(vertical = 12.adp)
+                .semantics {
+                    contentDescription = "Enter text to translate"
+                }
         )
 
-        if (sourceTextValue.text.isNotEmpty()) {
-            Box(modifier = Modifier.size(48.dp)) {
-                if (isLoading) PulseAnimation(
-                    modifier = Modifier.align(Alignment.Center),
-                    color = MaterialTheme.colorScheme.primary
-                )
-                androidx.compose.animation.AnimatedVisibility(
-                    visible = sourceTextValue.text.isNotEmpty(),
-                    modifier = Modifier.align(Alignment.Center),
-                    enter = fadeIn(tween(300)),
-                    exit = fadeOut(tween(300))
-                ) {
+        AnimatedVisibility(
+            visible = sourceTextValue.text.isNotEmpty(),
+            enter = fadeIn(tween(300)),
+            exit = fadeOut(tween(300))
+        ) {
+            Box(
+                modifier = Modifier
+                    .size(48.adp)
+                    .semantics {
+                        liveRegion = LiveRegionMode.Polite
+                    },
+                contentAlignment = Alignment.Center
+            ) {
+                if (isLoading) {
+                    PulseAnimation(
+                        color = MaterialTheme.colorScheme.primary,
+                        modifier = Modifier.semantics {
+                            contentDescription = "Translation in progress"
+                        }
+                    )
+                } else {
                     CustomButton(
                         icon = R.drawable.icon_clear,
                         contentDescription = stringResource(R.string.action_clear),
                         onClick = onClearText,
-                        iconSize = 18.dp,
-                        showBorder = false
+                        iconSize = 18.adp,
+                        showBorder = false,
+                        modifier = Modifier.semantics {
+                            contentDescription = "Clear input text"
+                        }
                     )
                 }
             }
         }
     }
-}
-
-@Preview(showBackground = true)
-@Composable
-private fun SourceTextSectionPreview() {
-    SourceTextSection(
-        sourceTextValue = TextFieldValue("Example Text"),
-        onTextChange = {},
-        onClearText = {},
-        isLoading = true
-    )
 }
