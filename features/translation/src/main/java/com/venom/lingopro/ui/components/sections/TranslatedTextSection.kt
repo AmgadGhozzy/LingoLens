545819c888daac5217db6a812d68e2adb954e6b3
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2026-02-02 22:54:16 +0200

    Replace dp with adp in TranslatedTextSection

--- features/translation/src/main/java/com/venom/lingopro/ui/components/sections/TranslatedTextSection.kt
+++ features/translation/src/main/java/com/venom/lingopro/ui/components/sections/TranslatedTextSection.kt
@@ -1,14 +1,17 @@
 package com.venom.lingopro.ui.components.sections
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.venom.resources.R
+import com.venom.ui.components.common.adp
 import com.venom.ui.components.inputs.AnimatedResponseTextField
 import com.venom.ui.components.other.TextShimmer
 
@@ -24,7 +27,7 @@ fun TranslatedTextSection(
         AnimatedResponseTextField(
             text = translatedTextValue.text,
             placeHolderText = stringResource(R.string.translate_result_placeholder),
-            modifier = modifier.fillMaxWidth().padding(vertical = 8.dp)
+            modifier = modifier.fillMaxWidth().padding(vertical = 8.adp)
         )
     }
 }
@@ -34,7 +37,7 @@ fun TranslatedTextSection(
 private fun TranslatedTextSectionPreview() {
     MaterialTheme {
         Column(
-            verticalArrangement = Arrangement.spacedBy(16.dp)
+            verticalArrangement = Arrangement.spacedBy(16.adp)
         ) {
             // Loading state preview
             TranslatedTextSection(
