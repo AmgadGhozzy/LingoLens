d00b7bc291fcf7c4751dd905362f4b67b955fd1d
Amgad Ghozzy <AmgadGhozzy@gmail.com>
2026-02-16 23:39:52 +0200

    Update LearningDatabase with new entities and DAOs

--- core/data/src/main/java/com/venom/data/local/database/LearningDatabase.kt
+++ core/data/src/main/java/com/venom/data/local/database/LearningDatabase.kt
@@ -2,36 +2,48 @@ package com.venom.data.local.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.venom.data.local.dao.QuizAttemptDao
+import com.venom.data.local.dao.UsageDailyDao
 import com.venom.data.local.dao.UserActivityDao
 import com.venom.data.local.dao.UserIdentityDao
 import com.venom.data.local.dao.UserProfileDao
 import com.venom.data.local.dao.UserWordProgressDao
 import com.venom.data.local.dao.WordMasterDao
+import com.venom.data.local.dao.XpEventDao
+import com.venom.data.local.entity.QuizAttemptEntity
+import com.venom.data.local.entity.UsageDailyEntity
 import com.venom.data.local.entity.UserActivityEntity
+import com.venom.data.local.entity.UserDeviceEntity
 import com.venom.data.local.entity.UserIdentityEntity
 import com.venom.data.local.entity.UserProfileEntity
+import com.venom.data.local.entity.UserSubscriptionEntity
 import com.venom.data.local.entity.UserWordProgressEntity
 import com.venom.data.local.entity.WordMasterEntity
+import com.venom.data.local.entity.XpEventEntity
 
-/**
- * Unified database for all learning-related entities.
- */
 @Database(
     entities = [
         WordMasterEntity::class,
         UserIdentityEntity::class,
         UserWordProgressEntity::class,
         UserActivityEntity::class,
-        UserProfileEntity::class
+        UserProfileEntity::class,
+        XpEventEntity::class,
+        QuizAttemptEntity::class,
+        UsageDailyEntity::class,
+        UserSubscriptionEntity::class,
+        UserDeviceEntity::class
     ],
-    version = 1,
+    version = 2,
     exportSchema = false
 )
-
 abstract class LearningDatabase : RoomDatabase() {
     abstract fun wordMasterDao(): WordMasterDao
     abstract fun userIdentityDao(): UserIdentityDao
     abstract fun userWordProgressDao(): UserWordProgressDao
     abstract fun userActivityDao(): UserActivityDao
     abstract fun userProfileDao(): UserProfileDao
+    abstract fun xpEventDao(): XpEventDao
+    abstract fun quizAttemptDao(): QuizAttemptDao
+    abstract fun usageDailyDao(): UsageDailyDao
 }
